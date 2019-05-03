522bb719e07ce2b2cf09a5dfffa0e32e04b8f92e
zyadelgohary <zyadelgohary@users.noreply.github.com>
2019-05-02 21:22:35 -0700

    Installed & set up firebase to app

--- ServicesDev/android/app/src/main/java/com/services/MainApplication.java
+++ ServicesDev/android/app/src/main/java/com/services/MainApplication.java
@@ -3,6 +3,7 @@ package com.services;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import io.invertase.firebase.RNFirebasePackage;
 import com.swmansion.reanimated.ReanimatedPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.imagepicker.ImagePickerPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFirebasePackage(),
             new ReanimatedPackage(),
             new RNGestureHandlerPackage(),
             new ImagePickerPackage(),
