d5c030dd64a6a2479850a0e1a238f7a950cb3ffd
zyadelgohary <zyadelgohary@users.noreply.github.com>
2019-07-06 22:04:17 -0700

    Created the HelpView higher order component

---
+++ ServicesDev/src/components/HelpView.js
@@ -0,0 +1,24 @@
+//This is a higher order component that should adapt to the keyboard coming up. It will utilize 
+//KeyboardAvoidingView to make sure the TextInput being typed into is visible, as well as making sure
+//that if the keyboard is clicked away from, it should hide. It will take care of the screen's background
+//color and everything
+import React from 'react';
+import { SafeAreaView, TouchableWithoutFeedback, KeyboardAvoidingView, Keyboard } from 'react-native';
+import screenStyle from 'config/styles/screenStyle';
+
+//Creates the higher order component
+const HelpViewHOC = (Comp) => {
+    return ({ children, ...props }) => (
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+            <KeyboardAvoidingView style={screenStyle.container}>
+                <Comp {...props}>
+                    {children}
+                </Comp>
+            </KeyboardAvoidingView>
+        </TouchableWithoutFeedback>
+    )
+}
+
+//Creats the component & exports it
+const HelpView = HelpViewHOC(SafeAreaView);
+export default HelpView;
