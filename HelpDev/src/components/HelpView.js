c8167ee0959991fd76f405d444c29c36189a859a
zyadelgohary <zyadelgohary@users.noreply.github.com>
2019-07-11 21:56:13 -0700

    Updated Gradle

--- HelpDev/src/components/HelpView.js
+++ HelpDev/src/components/HelpView.js
@@ -11,7 +11,7 @@ import screenStyle from 'config/styles/screenStyle';
 const HelpViewHOC = (Comp) => {
     return ({ children, ...props }) => (
         <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
-            <KeyboardAvoidingView style={screenStyle.container} enabled>
+            <KeyboardAvoidingView style={screenStyle.container} enabled behavior="padding">
                 <Comp {...props} >
                     {children}
                 </Comp>
