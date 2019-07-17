70974abb7de8b5c136f9c9fd7a53966eee4667b9
zyadelgohary <zyadelgohary@users.noreply.github.com>
2019-07-16 22:54:40 -0700

    Fixed issue with crash android

--- HelpDev/src/components/LoadingSpinner.js
+++ HelpDev/src/components/LoadingSpinner.js
@@ -1,18 +1,23 @@
 //This component will be the default loading spinner that should be user throughout the entire app
 import React, { Component } from 'react';
-import { View, ActivityIndicator } from 'react-native';
+import { View } from 'react-native';
+import { DotIndicator } from 'react-native-indicators';
+import Modal from "react-native-modal";
 import colors from 'config/colors';
 import PropTypes from 'prop-types';
 
 //The class that will render the spinner
 class LoadingSpinner extends Component {
     render() {
+        const { isVisible } = this.props;
         return (
             <View>
-                <ActivityIndicator
-                    animating={this.props.isVisible}
-                    size={'small'}
-                    color={colors.lightBlue} />
+                <Modal visible={isVisible} transparent={true}>
+                    <DotIndicator
+                        animating={isVisible}
+                        color={colors.lightBlue}
+                        count={3} />
+                </Modal>
             </View>
         );
     }
