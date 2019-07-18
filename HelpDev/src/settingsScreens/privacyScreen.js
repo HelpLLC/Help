f6ee81bedb1b8ee3b148b4a93104f1242fcd6ada
zyadelgohary <zyadelgohary@users.noreply.github.com>
2019-07-18 11:10:07 -0700

    Made Terms & Privacy Scrollable

--- HelpDev/src/settingsScreens/privacyScreen.js
+++ HelpDev/src/settingsScreens/privacyScreen.js
@@ -1,21 +1,15 @@
 //The screen will contain the entire privacy policy
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { Text } from 'react-native';
 import strings from 'config/strings';
 import { ScrollView } from 'react-native-gesture-handler';
-import HelpView from '../components/HelpView';
-import screenStyle from 'config/styles/screenStyle';
 
 class aboutScreen extends Component {
     render() {
         return (
-            <HelpView style={screenStyle.container}>
-                <ScrollView>
-                    <View>
-                        <Text>{strings.PrivacyPolicy}</Text>
-                    </View>
-                </ScrollView>
-            </HelpView>
+            <ScrollView>
+                <Text>{strings.PrivacyPolicy}</Text>
+            </ScrollView >
         )
     }
 }
