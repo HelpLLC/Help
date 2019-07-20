693528d5fbb338d7b979faf76da5ddfe1b8fa36b
zyadelgohary <zyadelgohary@users.noreply.github.com>
2019-07-19 18:22:27 -0700

    Made fontSize based on PixelRatio

--- HelpDev/config/styles/fontStyles.js
+++ HelpDev/config/styles/fontStyles.js
@@ -1,120 +1,128 @@
 //This class will represent all the font styles that should be used throughout the application
 //This includes font families, sizes, and colors.
 import colors from '../colors';
-import { StyleSheet, Platform } from 'react-native';
+import { StyleSheet, Platform, PixelRatio } from 'react-native';
 
 //Gets the correct font based on either iOS or Android
 const font = (Platform.OS === 'ios' ? 'Arial Rounded MT Bold' : 'sans-serif-medium');
+
+//Sets the font size
+let baseFontSize = (Platform.OS === 'android' ? 20 : 18);
+const pixelRatio = PixelRatio.get();
+if (pixelRatio < 3) {
+    baseFontSize = (Platform.OS === 'android' ? 16 : 13.5);
+}
+
 export default StyleSheet.create({
 
     //The style for all big text that will be colored black
     bigTextStyleBlack: {
         fontFamily: font,
-        fontSize: 24,
+        fontSize: baseFontSize * 1.2,
         color: colors.black
     },
 
     //The style for all big text that will be colored white
     bigTextStyleWhite: {
         fontFamily: font,
-        fontSize: 24,
+        fontSize: baseFontSize * 1.2,
         color: colors.white
     },
 
     //The style for all big text that will be colored blue
     bigTextStyleBlue: {
         fontFamily: font,
-        fontSize: 24,
+        fontSize: baseFontSize * 1.2,
         color: colors.lightBlue
     },
 
     //The style for main text that will be colored black
     mainTextStyleBlack: {
         fontFamily: font,
-        fontSize: 20,
+        fontSize: baseFontSize,
         color: colors.black
     },
 
     //The style for main text that will be colored blue
     mainTextStyleBlue: {
         fontFamily: font,
-        fontSize: 20,
+        fontSize: baseFontSize,
         color: colors.lightBlue
     },
 
     //The style for main text that will be colored white
     mainTextStyleWhite: {
         fontFamily: font,
-        fontSize: 20,
+        fontSize: baseFontSize,
         color: colors.white
     },
 
     //The style for main text that will be colored gray
     mainTextStyleGray: {
         fontFamily: font,
-        fontSize: 20,
+        fontSize: baseFontSize,
         color: colors.gray
     },
 
     //The style for main text that will be colored red
     mainTextStyleRed: {
         fontFamily: font,
-        fontSize: 20,
+        fontSize: baseFontSize,
         color: colors.red
     },
 
     //The style for all non-main text that is black
     subTextStyleBlack: {
         fontFamily: font,
-        fontSize: 16,
+        fontSize: baseFontSize * 0.75,
         color: colors.black
     },
 
     //The style for all non-main text that is blue
     subTextStyleBlue: {
         fontFamily: font,
-        fontSize: 16,
+        fontSize: baseFontSize * 0.75,
         color: colors.lightBlue
     },
 
     //The style for all non-main text that is red
     subTextStyleRed: {
         fontFamily: font,
-        fontSize: 16,
+        fontSize: baseFontSize * 0.75,
         color: colors.red
     },
 
     //The style for all non-main text that is gray
     subTextStyleGray: {
         fontFamily: font,
-        fontSize: 16,
+        fontSize: baseFontSize * 0.75,
         color: colors.gray
     },
 
     //The style for all non-main text that is white
     subTextStyleWhite: {
         fontFamily: font,
-        fontSize: 16,
+        fontSize: baseFontSize * 0.75,
         color: colors.white
     },
 
     //The style for the tab label at the bottom of the screens
     tabLabelStyle: {
         fontFamily: font,
-        fontSize: 16
+        fontSize: baseFontSize * 0.75
     },
 
     //The style for the text on the button on the report issue screen
     reportIssueButtonTextStyle: {
         fontFamily: font,
-        fontSize: 30,
+        fontSize: baseFontSize * 1.5,
         color: colors.white
     },
 
     //The style for all big blue title texts
     bigTitleStyleBlue: {
         fontFamily: font,
-        fontSize: 88,
+        fontSize: baseFontSize * 4.4,
         color: colors.lightBlue
     },
 
