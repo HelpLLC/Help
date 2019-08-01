fb8a6536f02d1081664e5cd609388e64259fb28d
Zyad <zyadgohary@hotmail.com>
2019-07-31 17:20:04 -0700

    Fixed last PR merge

--- HelpDev/node_modules/react-native-gifted-chat/lib/utils.d.ts
+++
@@ -1,3 +0,0 @@
-import { IMessage } from './types';
-export declare function isSameDay(currentMessage: IMessage, diffMessage: IMessage | null | undefined): boolean;
-export declare function isSameUser(currentMessage: IMessage, diffMessage: IMessage | null | undefined): boolean;
