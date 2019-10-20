bbd8bbfd513116737b3d7d36852da363bb6f0c3f
Zyad <zyadgohary@hotmail.com>
2019-10-19 19:21:45 -0700

    Completed Issue #198

--- Dev/src/requesterScreens/categoryScreen.js
+++ Dev/src/requesterScreens/categoryScreen.js
@@ -1,42 +1,54 @@
+//This screen represents the screen that will show a specfic category
 import React, { Component } from 'react';
-import { View, Platform, Text, Dimensions, Image } from 'react-native';
-import fontStyles from 'config/styles/fontStyles';
-import { check, request, PERMISSIONS, RESULTS } from 'react-native-permissions';
-import strings from 'config/strings';
+import { View, Dimensions } from 'react-native';
 import FirebaseFunctions from '../../config/FirebaseFunctions';
 import LoadingSpinner from '../components/LoadingSpinner';
 import screenStyle from 'config/styles/screenStyle';
-import Geolocation from 'react-native-geolocation-service';
 import HelpView from '../components/HelpView';
 import ServicesList from '../components/ServicesList';
-import LeftMenu from './LeftMenu';
-import SideMenu from 'react-native-side-menu';
 import TopBanner from '../components/TopBanner';
 
 class categoryScreen extends Component {
+
   state = {
-    isOpen: false
+    isOpen: false,
+    isLoading: true,
+    products: ""
   };
-  render() {
-    //Filters the products and removes any that are posted by blocked users
-    let {
-      allProducts,
-      requester,
-      categoryName
-    } = this.props.navigation.state.params;
-    
-    let products = FirebaseFunctions.getProductsByCategory(
-      allProducts,
-      categoryName
-    );
+
+  //Fetches the products for this category
+  async componentDidMount() {
+
+    const { allProducts, categoryName, requester } = this.props.navigation.state.params;
+    let products = await FirebaseFunctions.getProductsByCategory(allProducts, categoryName);
     products = products.filter(product => {
       return !requester.blockedUsers.includes(product.offeredByID);
     });
+    this.setState({
+      products,
+      isLoading: false
+    });
 
-    
+    return 0;
+
+  }
+
+  render() {
+
+    //Fetches the correct 
+    const { requester, categoryName } = this.props.navigation.state.params;
+    const { products } = this.state;
+
+    if (this.state.isLoading === true) {
+      return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <LoadingSpinner isVisible={true} />
+        </View>
+      )
+    }
+
     return (
       <HelpView style={screenStyle.container}>
-
         <TopBanner
           title={categoryName}
           leftIconName='angle-left'
