a33d05296c1e9ef5281aeec7caecfefdfbf354ca
Zyad <zyadgohary@hotmail.com>
2019-10-31 13:52:06 -0700

    Fixed error in provider creation

--- Dev/src/firstScreens/providerAdditionalInformationScreen.js
+++ Dev/src/firstScreens/providerAdditionalInformationScreen.js
@@ -79,6 +79,7 @@ export default class providerAdditionalInformationScreen extends Component {
 					companyName: businessName,
 					companyDescription: businessInfo,
 					isVerified: false,
+					serviceIDs: [],
 					phoneNumber,
 					website,
 					location,
