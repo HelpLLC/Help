9819bf4e402fdd0f4df2adaced7fc7786b9907ff
Zyad <zyadgohary@hotmail.com>
2019-10-30 21:49:38 -0700

    Got rid of unused resources

--- Dev/config/images/images.js
+++ Dev/config/images/images.js
@@ -2,6 +2,4 @@
 export default {
     LawnMowing: require('../images/lawn_mowing.jpg'),
     BlankWhite: require('../images/blank_white.jpg'),
-    PressureWashing: require('../images/pressure_washing.jpg'),
-    LeaveRaking: require('../images/leave_raking.jpg'),
 }
