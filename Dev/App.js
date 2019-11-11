b12e3ef2bd478889cf968f256f4e7b78d33622d3
zyadelgohary <zyadelgohary@users.noreply.github.com>
2019-11-11 10:35:04 -0800

    Added updater

--- Dev/App.js
+++ Dev/App.js
@@ -1,32 +1,131 @@
 //This is the "main method" for the Help! applications. This component is launched and with it,
 //the FirstScreenNavigator which connects to the rest of the screens.
 import React, { Component } from 'react';
-import MainStackNavigator from './src/MainStackNavigator';
-import { YellowBox } from 'react-native';
+import strings from 'config/strings';
+import { YellowBox, View, Linking, Platform } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
+import screenStyle from 'config/styles/screenStyle';
+import HelpView from './src/components/HelpView';
+import ErrorAlert from './src/components/ErrorAlert';
+import firebase from 'react-native-firebase';
 import codePush from 'react-native-code-push';
 
 //Launches the app with the persisted store
 class App extends Component {
-	async componentDidMount() {
-		codePush.notifyAppReady();
-	}
-
-	render() {
 		//Ignores a specific warning
-		YellowBox.ignoreWarnings([
-			'ViewPagerAndroid',
-			'componentWillMount',
-			'componentWillReceiveProps'
-		]);
-		return <MainStackNavigator />;
-	}
+  state = {
+    isLoading: true
+  };
+
+  /*
+   * The following are all methods for registering the user's device with notifications
+   * services
+   */
+  async checkPermission() {
+    const enabled = await firebase.messaging().hasPermission();
+    if (enabled) {
+      await this.getToken();
+      return 0;
+    } else {
+      await this.requestPermission();
+      return 0;
+    }
+  }
+
+  async getToken() {
+    let fcmToken = await AsyncStorage.getItem('fcmToken');
+    if (!fcmToken) {
+      fcmToken = await firebase.messaging().getToken();
+      if (fcmToken) {
+        // user has a device token
+        await AsyncStorage.setItem('fcmToken', fcmToken);
+        return 0;
+      }
+      return 0;
+    }
+    return 0;
+  }
+
+  //2
+  async requestPermission() {
+    try {
+      await firebase.messaging().requestPermission();
+      // User has authorized
+      await this.getToken();
+      return 0;
+    } catch (error) {
+      // User has rejected permissions
+      return 0;
+    }
+  }
+
+  async createNotificationListeners() {
+    /*
+     * Triggered when a particular notification has been received in foreground
+     * */
+    this.notificationListener = firebase.notifications().onNotification((notification) => {});
+
+    /*
+     * If your app is in background, you can listen for when a notification is clicked / tapped / opened as follows:
+     * */
+    this.notificationOpenedListener = firebase
+      .notifications()
+      .onNotificationOpened((notificationOpen) => {});
+
+    /*
+     * If your app is closed, you can check if it was opened by a notification being clicked / tapped / opened as follows:
+     * */
+    const notificationOpen = await firebase.notifications().getInitialNotification();
+    if (notificationOpen) {
+    }
+    /*
+     * Triggered for data only payload in foreground
+     * */
+    this.messageListener = firebase.messaging().onMessage((message) => {});
+  }
+
+  async componentWillUnmount() {
+    await this.notificationListener();
+    await this.notificationOpenedListener();
+  }
+
+  async componentDidMount() {
+    this.setState({ isLoading: false });
+  }
+
+  render() {
+    //Ignores a specific warning
+    YellowBox.ignoreWarnings([
+      'ViewPagerAndroid',
+      'componentWillMount',
+      'componentWillReceiveProps'
+    ]);
+    return (
+      <HelpView style={screenStyle.container}>
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <ErrorAlert
+					isVisible={true}
+					onPress={() => {
+						if (Platform.OS === 'ios') {
+              Linking.openURL('itms-apps://itunes.apple.com/app/apple-store/id1468626210?mt=8');
+            } else if (Platform.OS === 'android') {
+              Linking.openURL('market://details?id=com.Help.Help');
+            }
+					}}
+					title={strings.UpdateAvailable}
+					message={strings.UpdateAvailableMessage}
+				/>
+        </View>
+      </HelpView>
+    );
+  }
 }
 
 //Configures the app to connect with the code push server in order to check for updates on app
 //resume from backgroud
 let codePushOptions = {
-	checkFrequency: codePush.CheckFrequency.ON_APP_RESUME,
-	installMode: codePush.InstallMode.ON_NEXT_RESTART
+  checkFrequency: codePush.CheckFrequency.ON_APP_RESUME,
+  installMode: codePush.InstallMode.ON_NEXT_RESTART
 };
 App = codePush(codePushOptions)(App);
 
