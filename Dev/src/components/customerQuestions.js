fc9df9b7bc39bca07a0724e8307feb094090f498
Bilal <belmohandes@outlook.com>
2019-11-22 18:27:21 -0800

    Finished Customer side

--- Dev/src/components/customerQuestions.js
+++ Dev/src/components/customerQuestions.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react"
-import { View, Text, Dimensions } from 'react-native';
+import { View, Text, Dimensions, } from 'react-native';
 import fontStyles from "../../config/styles/fontStyles";
 import MultiLineRoundedBoxInput from '../components/MultiLineRoundedBoxInput';
 
@@ -7,24 +7,35 @@ class CustomerQuestions extends Component {
 
 
 
+
     render() {
         const questionsList = [];
-        const answers = [];
-        const {product} = this.props;
+        const { product } = this.props;
         const questions = product.questions;
 
-        for(let i = 0; i < questions.length; i++){
+        const {answers} = [];
+
+        for (let i = 0; i < questions.length; i++) {
+
             questionsList.push(
-                <View>
-                    <Text style={fontStyles.mainTextStyleBlack}>{questions[i]}</Text>
-                    <MultiLineRoundedBoxInput
-							width={Dimensions.get('window').width * 0.6}
-							height={Dimensions.get('window').height * 0.1}
-							placeholder={'Please Answer the question here'}
-							onChangeText={(input) => onChangeText(input)}
-							value={}
-							maxLength={350}
-						/>
+                <View key={i}>
+                    <View style={{ marginLeft: Dimensions.get('window').width * .03, marginTop: Dimensions.get('window').height * .01 }}>
+                        <Text style={fontStyles.mainTextStyleBlack}>{questions[i]}</Text>
+                    </View>
+                    <View style={{ marginLeft: Dimensions.get('window').width * .02, marginTop: Dimensions.get('window').height * .01 }}>
+                        <MultiLineRoundedBoxInput
+                            width={Dimensions.get('window').width * 0.8}
+                            height={Dimensions.get('window').height * 0.08}
+                            placeholder={'Please answer the question here'}
+                            onChangeText={(input) => {
+                                answers[i] = input;
+                                this.props.onGetAnswer(answers);
+                            }}
+                            value={answers[i]}
+                            maxLength={350}
+                        />
+
+                    </View>
                 </View>
             )
         }
--- Dev/src/requesterScreens/customerQuestionsScreen.js
+++ Dev/src/requesterScreens/customerQuestionsScreen.js
@@ -1,18 +1,40 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Dimensions, FlatList } from 'react-native';
 import HelpView from '../components/HelpView';
 import TopBanner from '../components/TopBanner';
 import strings from '../../config/strings';
 import CustomerQuestions from '../components/customerQuestions'
+import RoundBlueButton from '../components/RoundBlueButton';
+import roundBlueButtonStyle from '../../config/styles/componentStyles/roundBlueButtonStyle';
+import fontStyles from '../../config/styles/fontStyles'
+import { ScrollView } from 'react-native-gesture-handler';
+import MultiLineRoundedBoxInput from '../components/MultiLineRoundedBoxInput'
 
 class CustomerQuestionsScreen extends Component {
 
     state = {
-        isLoading: false
+        isLoading: false,
+        answers: []
+    }
+
+    async componentDidMount() {
+        const { product, requester } = this.props.navigation.state.params;
+        this.setState({
+            product,
+            requester
+        })
+
+    }
+
+    saveAnswers() {
+        const answers = this.state.answers;
     }
 
     render() {
-        const {product} = this.props.navigation.state.params; 
+        const { product } = this.props.navigation.state.params;
+        const answers = [];
+        const questions = product.questions;
+
         return (
             <HelpView>
                 <TopBanner
@@ -20,7 +42,47 @@ class CustomerQuestionsScreen extends Component {
                     leftIconName='angle-left'
                     leftOnPress={() => this.props.navigation.goBack()}
                 />
-                <CustomerQuestions product={product}/>
+                <View style={{ height: Dimensions.get('window').height * .7 }}>
+                    <FlatList
+                        showsHorizontalScrollIndicator={false}
+                        showsVerticalScrollIndicator={false}
+                        data={questions}
+                        numColumns={1}
+                        keyExtractor={item => item.categoryName}
+                        showsVerticalScrollIndicator={false}
+                        renderItem={({ item, index }) => (
+                            <View key={index}>
+                                <View style={{ marginLeft: Dimensions.get('window').width * .03, marginTop: Dimensions.get('window').height * .01 }}>
+                                    <Text style={fontStyles.mainTextStyleBlack}>{questions[index]}</Text>
+                                </View>
+                                <View style={{ marginLeft: Dimensions.get('window').width * .02, marginTop: Dimensions.get('window').height * .01 }}>
+                                    <MultiLineRoundedBoxInput
+                                        width={Dimensions.get('window').width * 0.8}
+                                        height={Dimensions.get('window').height * 0.08}
+                                        placeholder={'Please answer the question here'}
+                                        onChangeText={(input) => {
+                                            answers[index] = input;
+                                            this.setState({
+                                                answers
+                                            });
+                                        }}
+                                        value={answers[i]}
+                                        maxLength={350}
+                                    />
+
+                                </View>
+                            </View>
+                        )}
+                    />
+                </View>
+                <RoundBlueButton
+                    title={strings.Request}
+                    style={roundBlueButtonStyle.MediumSizeButton}
+                    textStyle={fontStyles.bigTextStyleWhite}
+                    onPress={() => {
+                        this.saveAnswers();
+                    }}
+                />
             </HelpView>
         )
     }
