550f85582654d15763f75f5a24c609e7be26b04d
Zyad <zyadgohary@hotmail.com>
2019-11-24 23:01:44 -0800

    Added ErrorAlert

--- Dev/App.js
+++ Dev/App.js
@@ -4,10 +4,12 @@ import React, { Component } from 'react';
 import MainStackNavigator from './src/MainStackNavigator';
 import { YellowBox } from 'react-native';
 import codePush from 'react-native-code-push';
+import { View, Linking, Platform } from 'react-native';
+import ErrorAlert from './src/components/ErrorAlert';
+import strings from 'config/strings'; 
 
 //Launches the app with the persisted store
 class App extends Component {
-
 	render() {
 		//Ignores a specific warning
 		YellowBox.ignoreWarnings([
@@ -15,7 +17,22 @@ class App extends Component {
 			'componentWillMount',
 			'componentWillReceiveProps'
 		]);
-		return <MainStackNavigator />;
+		return (
+			<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+				<ErrorAlert
+					isVisible={true}
+					onPress={() => {
+						if (Platform.OS === 'ios') {
+							Linking.openURL('itms-apps://itunes.apple.com/app/apple-store/id1468626210?mt=8');
+						} else if (Platform.OS === 'android') {
+							Linking.openURL('market://details?id=com.Help.Help');
+						}
+					}}
+					title={strings.UpdateAvailable}
+					message={strings.UpdateAvailableMessage}
+				/>
+			</View>
+		);
 	}
 }
 
