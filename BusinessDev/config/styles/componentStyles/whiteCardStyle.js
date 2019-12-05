ca09e759f79baec041906b083d7af6f6dabbaf24
Zyad <zyadgohary@hotmail.com>
2019-12-04 22:16:44 -0800

    Removed unneeded component styles

--- BusinessDev/config/styles/componentStyles/whiteCardStyle.js
+++ BusinessDev/config/styles/componentStyles/whiteCardStyle.js
@@ -3,14 +3,13 @@ import colors from '../../colors';
 import { Dimensions } from 'react-native';
 
 export default {
-
-    //This is the card that appears in the about section & the settings screen
-    whiteCardStyle: {
-        height: Dimensions.get('window').height * 0.07,
-        width: Dimensions.get('window').width - 30,
-        backgroundColor: colors.white,
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-between'
-    }
-}
+	//This is the card that appears in the about section & the settings screen
+	whiteCardStyle: {
+		height: Dimensions.get('window').height * 0.07,
+		width: Dimensions.get('window').width - 30,
+		backgroundColor: colors.white,
+		flexDirection: 'row',
+		alignItems: 'center',
+		justifyContent: 'space-between'
+	}
+};
