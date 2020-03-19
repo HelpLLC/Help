7dc282d996a1caf64e3caadac3597fb8fc6703f6
Zyad <zyadgohary@hotmail.com>
2020-03-18 21:28:26 -0700

    Fixed styling issue with @khalidelho

--- WebsiteDev/src/components/BusinessServiceCard.js
+++ WebsiteDev/src/components/BusinessServiceCard.js
@@ -1,12 +1,8 @@
 import React, { Component } from "react";
-import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
-import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
-import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
-import CardMedia from "@material-ui/core/CardMedia";
-import "../components/BusinessServiceCard.css";
+import './BusinessServiceCard.css';
 import { Divider } from "@material-ui/core";
 import image from "../images/download.jpg";
 import fontStyles from "../config/fontStyles.js";
