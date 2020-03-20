b6fe70f4669abf74a274e20cd81f87b5c8250246
Zyad <zyadgohary@hotmail.com>
2020-03-20 15:26:27 -0700

    Fixed issues with class declaration

---
+++ WebsiteDev/src/business/BusinessScreens/CreateProduct/CreateProductScreen.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import HelpButton from '../../../components/HelpButton';
+import EditText from '../../../components/EditText';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import './CreateProductScreen.css';
+import { Divider } from '@material-ui/core';
+import fontStyles from '../../../config/fontStyles';
+import { FaBeer } from 'react-icons/fa';
+
+export default class CreateProductScreen extends Component {
+	render() {
+		return (
+			<div>
+				<h1>Hello World</h1>
+			</div>
+		);
+	}
+}
