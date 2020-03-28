10173e2159a55e61e3d579531cc18ca90d0983cd
Zyad <zyadgohary@hotmail.com>
2020-03-28 14:57:11 -0700

    Left over commits

--- WebsiteDev/src/business/StartingScreens/Authentication/Login.js
+++ WebsiteDev/src/business/StartingScreens/Authentication/Login.js
@@ -5,12 +5,10 @@ import FirebaseFunctions from '../../../config/FirebaseFunctions';
 import HelpButton from '../../../components/HelpButton.js';
 import TitleComponent from '../../../components/TitleComponent.js';
 import EditText from '../../../components/EditText.js';
-import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
 
 export function Login() {
 	const [email, setEmail] = React.useState('');
