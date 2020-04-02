fdbf80606534bdfcf37907c5f2bf23e543597fd2
shreshthkharbanda <shreshthkharbanda1@gmail.com>
2020-04-02 01:58:31 -0700

    Retreived analytics from firebase and formatted them into graphs

--- WebsiteDev/src/business/BusinessScreens/Analytics/Analytics.js
+++ WebsiteDev/src/business/BusinessScreens/Analytics/Analytics.js
@@ -4,9 +4,160 @@ import TitleComponent from '../../../components/TitleComponent';
 import colors from '../../../config/colors';
 import fontStyles from '../../../config/fontStyles';
 import { Chart } from 'react-google-charts';
+import FirebaseFunctions from '../../../config/FirebaseFunctions';
 
 export default class Analytics extends Component {
+	state = {
+		isScreenLoading: true,
+		revenueBy: 'By Month',
+		customerLocationsBy: 'By City',
+		topServicesBy: 'By Total Requests',
+		revenueData: '',
+		customerLocationData: '',
+		topServicesData: '',
+		myRevenue: [[]],
+		myServices: [[]],
+		myLocations: [[]]
+	};
+
+	async componentDidMount() {
+		// const { businessID } = 'zjCzqSiCpNQELwU3ETtGBANz7hY2';
+		const analyticsData = await FirebaseFunctions.call('getBusinessAnalyticsByBusinessID', {
+			businessID: 'zjCzqSiCpNQELwU3ETtGBANz7hY2'
+		});
+		const revenueData = analyticsData[0];
+		const topServicesData = analyticsData[1];
+		const customerLocationData = analyticsData[2];
+		this.setState({
+			revenueData,
+			customerLocationData,
+			topServicesData,
+			isScreenLoading: false
+		});
+
+		const revenueChart = await this.generateRevenueChartData();
+		this.setState({ myRevenue: revenueChart });
+		const servicesChart = await this.generateTopServicesChartData();
+		this.setState({ myServices: servicesChart });
+		const locationsChart = await this.generateTopLocationsChartData();
+		this.setState({ myLocations: locationsChart });
+	}
+
+	//Generates the chart data for the revenue graph
+	async generateRevenueChartData() {
+		const { revenueBy, revenueData } = this.state;
+
+		const monthStrings = [
+			'Jan',
+			'Feb',
+			'Mar',
+			'Apr',
+			'May',
+			'Jun',
+			'Jul',
+			'Aug',
+			'Sep',
+			'Oct',
+			'Nov',
+			'Dec'
+		];
+
+		let months = Object.keys(revenueData);
+		months.sort();
+		months.reverse();
+		let chartData = [['Month', 'Revenue ($)']];
+		months = months.filter((value) => value.includes('-'));
+		months = months.filter((value, index) => index <= 11);
+
+		var data = [];
+		for (let i = months.length - 1; i >= 0; i--) {
+			const month = months[i];
+			data.push(parseInt(revenueData[month]));
+		}
+		console.log(data);
+
+		if (months.length > 4) {
+			for (let i = months.length - 2; i >= 0; i--) {
+				const month = months[i];
+				const monthString = monthStrings[parseInt(month.substring(month.indexOf('-') + 1)) - 1];
+				const yearString = month.substring(0, month.indexOf('-'));
+				chartData.push(new Array(monthString + ' ' + yearString, data[i]));
+			}
+		} else {
+			for (let i = months.length - 1; i >= 0 && chartData.length < 3; i--) {
+				const month = months[i];
+				const monthString = monthStrings[parseInt(month.substring(month.indexOf('-') + 1)) - 1];
+				const yearString = month.substring(0, month.indexOf('-'));
+				chartData.push(new Array(monthString + ' ' + yearString, data[i]));
+			}
+		}
+
+		return { chartData };
+	}
+
+	//Generates the chart data for the services graph
+	async generateTopServicesChartData() {
+		const { topServicesBy, topServicesData } = this.state;
+
+		let services = Object.keys(topServicesData);
+
+		let chartData = [['Services', 'Orders']];
+
+		// Ordered by total number of requests
+		services.sort((a, b) => {
+			return topServicesData[b].totalRequests - topServicesData[a].totalRequests;
+		});
+		for (let i = 0; i < services.length && i < 3; i++) {
+			chartData.push(
+				new Array(
+					topServicesData[services[i]].serviceTitle,
+					topServicesData[services[i]].totalRequests
+				)
+			);
+		}
+
+		return { chartData };
+	}
+
+	//Generates the chart data for the customer locations graph
+	async generateTopLocationsChartData() {
+		const { customerLocationsBy, customerLocationData } = this.state;
+
+		let chartData = [['Services', 'Orders']];
+		const { Cities } = customerLocationData;
+		let cityKeys = Object.keys(Cities);
+		cityKeys.sort((a, b) => Cities[b] - Cities[a]);
+		for (let i = 0; i < cityKeys.length && i < 3; i++) {
+			chartData.push(new Array(cityKeys[i], Cities[cityKeys[i]]));
+		}
+
+		return { chartData };
+	}
+
 	render() {
+		const {
+			revenueBy,
+			customerLocationsBy,
+			topServicesBy,
+			isScreenLoading,
+			myRevenue,
+			myServices,
+			myLocations
+		} = this.state;
+		if (isScreenLoading === true) {
+			return (
+				<div className='container'>
+					<TitleComponent />
+					<TitleComponent text={'Analytics'} isCentered={true} textColor={colors.lightBlue} />
+					<TitleComponent
+						text={'Screen loading...'}
+						isCentered={true}
+						textColor={colors.lightBlue}
+					/>
+				</div>
+			);
+		}
+
 		return (
 			<div className='container'>
 				<TitleComponent />
@@ -19,22 +170,9 @@ export default class Analytics extends Component {
 				<Chart
 					width={'auto'}
 					chartType='Line'
+					style={{ marginRight: '1%', marginLeft: '5%' }}
 					loader={<div>Loading Chart</div>}
-					data={[
-						['Month', 'Revenue ($)'],
-						['January', 46000],
-						['Feburary', 48000],
-						['March', 52000],
-						['April', 54000],
-						['May', 56000],
-						['June', 60000],
-						['July', 64500],
-						['August', 65500],
-						['September', 69000],
-						['October', 73000],
-						['November', 75000],
-						['December', 78000]
-					]}
+					data={myRevenue.chartData}
 					options={{
 						height: 350,
 						hAxis: {
@@ -56,40 +194,25 @@ export default class Analytics extends Component {
 					width={'auto'}
 					chartType='LineChart'
 					loader={<div>Loading Chart</div>}
-					data={[
-						['Month', 'Revenue ($)'],
-						['January', 46000],
-						['Feburary', 48000],
-						['March', 52000],
-						['April', 54000],
-						['May', 56000],
-						['June', 60000],
-						['July', 64500],
-						['August', 65500],
-						['September', 69000],
-						['October', 73000],
-						['November', 75000],
-						['December', 78000]
-					]}
+					data={myRevenue.chartData}
 					options={{
 						height: 350,
 						fontFamily: 'Arial',
-						fontSize: 14,
 						chart: {
-							title: 'Monthly Revenue in 2020',
+							title: 'Monthly Revenue in 2020'
 						},
 						hAxis: {
 							title: 'Month',
-							textStyle: fontStyles.subTextStyleBlue,
+							textStyle: fontStyles.miniTextStyleBlue
 						},
 						vAxis: {
 							title: 'Revenue ($)',
-							textStyle: fontStyles.subTextStyleBlue,
+							textStyle: fontStyles.miniTextStyleBlue
 						},
 						colors: [colors.lightBlue],
 						series: {
 							0: { lineWidth: 3 }
-						},
+						}
 					}}
 				/>
 
@@ -101,16 +224,10 @@ export default class Analytics extends Component {
 				<Chart
 					width={'auto'}
 					height={'75%'}
+					style={{ marginRight: '1%', marginLeft: '5%' }}
 					chartType='Bar'
 					loader={<div>Loading Chart</div>}
-					data={[
-						['Services', 'Orders'],
-						['Home Cleaning', 300],
-						['Painting', 280],
-						['Carpeting', 260],
-						['Windows', 230],
-						['Tiling', 200]
-					]}
+					data={myServices.chartData}
 					options={{
 						colors: [colors.lightBlue]
 					}}
@@ -124,25 +241,58 @@ export default class Analytics extends Component {
 				<Chart
 					chartType='ColumnChart'
 					width='auto'
-					data={[
-						['Service', 'Orders'],
-						['Home Cleaning', 300],
-						['Painting', 280],
-						['Carpeting', 260],
-						['Windows', 230],
-						['Tiling', 200]
-					]}
+					data={myServices.chartData}
 					options={{
 						height: 350,
 						hAxis: {
 							title: 'Services',
-							textStyle: fontStyles.subTextStyleBlue,
+							textStyle: fontStyles.subTextStyleBlue
 						},
 						vAxis: {
 							title: 'Orders',
-							textStyle: fontStyles.subTextStyleBlue,
+							textStyle: fontStyles.subTextStyleBlue
 						},
-						colors: [colors.lightBlue],
+						colors: [colors.lightBlue]
+					}}
+				/>
+
+				<TitleComponent
+					text={'Top Locations (Material UI)'}
+					isCentered={true}
+					textColor={colors.lightBlue}
+				/>
+				<Chart
+					width={'auto'}
+					height={'75%'}
+					style={{ marginRight: '1%', marginLeft: '5%' }}
+					chartType='Bar'
+					loader={<div>Loading Chart</div>}
+					data={myLocations.chartData}
+					options={{
+						colors: [colors.lightBlue]
+					}}
+				/>
+
+				<TitleComponent
+					text={'Top Locations (Normal)'}
+					isCentered={true}
+					textColor={colors.lightBlue}
+				/>
+				<Chart
+					chartType='ColumnChart'
+					width='auto'
+					data={myLocations.chartData}
+					options={{
+						height: 350,
+						hAxis: {
+							title: 'Services',
+							textStyle: fontStyles.subTextStyleBlue
+						},
+						vAxis: {
+							title: 'Orders',
+							textStyle: fontStyles.subTextStyleBlue
+						},
+						colors: [colors.lightBlue]
 					}}
 				/>
 
@@ -217,7 +367,7 @@ export default class Analytics extends Component {
 								strokeWidth: 4
 							},
 							dayOfWeekLabel: fontStyles.mainTextStyleBlue,
-							 dayOfWeekRightSpace: 10,
+							dayOfWeekRightSpace: 10,
 							monthLabel: fontStyles.mainTextStyleBlue,
 							monthOutlineColor: {
 								stroke: '#000',
