250f98e41d99ce0d1bcd22a3e45be4b0808cedaf
KhalidPM <45704194+KhalidPM@users.noreply.github.com>
2020-04-02 22:21:51 -0700

    App.js fix

--- WebsiteDev/src/App.js
+++ WebsiteDev/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import { BrowserRouter } from "react-router-dom";
 import LandingPageNavigator from "./business/StartingScreens/LandingPage/LandingPageNavigator";
-import Calendar from "./business/BusinessScreens/Calender/Calender";
-import BusinessServiceCard from "./components/BusinessServiceCard";
 
 export default class App extends Component {
   render() {
-    return <BusinessServiceCard />;
+    return (
+      <BrowserRouter>
+        <LandingPageNavigator />
+      </BrowserRouter>
+    );
   }
 }
