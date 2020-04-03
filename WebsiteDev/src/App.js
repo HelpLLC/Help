2a7ddc71c816823403c9414808edfaa5af31d18d
KhalidPM <45704194+KhalidPM@users.noreply.github.com>
2020-04-02 22:15:42 -0700

    Added All XD files to design folder

--- WebsiteDev/src/App.js
+++ WebsiteDev/src/App.js
@@ -1,13 +1,11 @@
-import React, { Component } from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import LandingPageNavigator from './business/StartingScreens/LandingPage/LandingPageNavigator';
+import React, { Component } from "react";
+import { BrowserRouter } from "react-router-dom";
+import LandingPageNavigator from "./business/StartingScreens/LandingPage/LandingPageNavigator";
+import Calendar from "./business/BusinessScreens/Calender/Calender";
+import BusinessServiceCard from "./components/BusinessServiceCard";
 
 export default class App extends Component {
-	render() {
-		return (
-			<BrowserRouter>
-				<LandingPageNavigator/>
-			</BrowserRouter>
-		);
-	}
+  render() {
+    return <BusinessServiceCard />;
+  }
 }
