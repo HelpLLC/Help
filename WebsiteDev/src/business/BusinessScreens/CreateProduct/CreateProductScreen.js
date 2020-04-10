7dfae1f32d18c8badcd6dd076bb57dca686d1798
bilalelmohandes <52120027+bilalelmohandes@users.noreply.github.com>
2020-04-10 16:36:48 -0700

    Styled Create Product

--- WebsiteDev/src/business/BusinessScreens/CreateProduct/CreateProductScreen.js
+++ WebsiteDev/src/business/BusinessScreens/CreateProduct/CreateProductScreen.js
@@ -16,150 +16,125 @@ import DropdownButton from "react-bootstrap/DropdownButton";
 
 export default function CreateProductScreen() {
   return (
-    <section style={fontStyles.mainTextStyleBlack}>
-      <h2 className="header">Create Service</h2>
-      <section class="div">
-        <h2 className="heading">Get to know your service!</h2>
-        <Card class="container">
-          <CardContent class="sscontent">
-            <Typography>Service Name</Typography>
-            <EditText
-              class="input"
-              type="name"
-              labelText={"Enter title here"}
-              widthPercent={200}
-            />
+    <div>
+      <h2 className="createproductheading">Create Service</h2>
+      <div class="createproductcontainer">
+        <div>
+          <h3>Service Information</h3>
+        </div>
+        <div>
+          <Typography>Service Name</Typography>
+          <EditText
+            class="createproductinput"
+            type="name"
+            labelText={"Enter title here"}
+            widthPercent={200}
+          />
+        </div>
+        <div>
+          <Typography>Service Description</Typography>
+          <EditText
+            className="createproductinput"
+            labelText={"Give a little description for your customers to see..."}
+            widthPercent={300}
+          />
+        </div>
+
+        <div>
+          <Typography class="createproducttitles">Price</Typography>
+          <CardContent class="createproductpriceinput">
+            <DropdownButton
+              id="createproductdropdown-basic-button"
+              title="rate"
+            >
+              <Dropdown.Item>per</Dropdown.Item>
+              <Dropdown.Item>Test</Dropdown.Item>
+            </DropdownButton>
+            <EditText class="createproductsecondpriceinput" widthPercent={50} />
           </CardContent>
-          <CardContent class="sscontent">
-            <Typography>Service Description</Typography>
+        </div>
+        <div>
+          <h3>Select what you need from the customer.</h3>
+        </div>
+        <div className="buttonlistcontainer">
+          <div>
+            <HelpButton fullWidth={false} label="Phone Number" />
+          </div>
+          <div class="productbuttoncontainer">
+            <HelpButton fullWidth={false} label="Email" />
+          </div>
+          <div class="productbuttoncontainer">
+            <HelpButton fullWidth={false} label="Address" />
+          </div>
+        </div>
+        <div class="textmorespace">
+          <h3>Any Additional Requests?</h3>
+        </div>
+        <div>
+          <Typography>Question 1</Typography>
+          <div className="createproducticondiv">
             <EditText
-              class="input"
-              labelText={
-                "Give a little description for your customers to see..."
-              }
+              class="createproductinput"
+              labelText={"Enter Question Here"}
               widthPercent={300}
             />
-          </CardContent>
-          <CardContent class="sscontent">
-            <Typography class="titles">Price</Typography>
-            <CardContent class="priceinput">
-              <DropdownButton id="dropdown-basic-button" title="rate">
-                <Dropdown.Item>per</Dropdown.Item>
-                <Dropdown.Item>Test</Dropdown.Item>
-              </DropdownButton>
-              <EditText class="secondpriceinput" widthPercent={50} />
-            </CardContent>
-          </CardContent>
-        </Card>
-      </section>
-      <section className="section">
-        <IconContext.Provider value={{ className: "react-icons" }}>
-          <div>
-            <FaArrowDown size="5em" vertical-align="middle" />
+            <FaTrash />
           </div>
-        </IconContext.Provider>
-      </section>
-      <section class="div">
-        <h2 className="heading">
-          What information should your customer provide you?
-        </h2>
-        <Card class="container">
-          <CardContent class="content">
-            <HelpButton fullWidth={false} label="Phone Number" />
-          </CardContent>
-          <CardContent class="content">
-            <HelpButton fullWidth={false} label="Email" />
-          </CardContent>
-          <CardContent class="content">
-            <HelpButton fullWidth={false} label="Address" />
-          </CardContent>
-        </Card>
-        <section className="section">
-          <IconContext.Provider value={{ className: "react-icons" }}>
-            <div>
-              <FaArrowDown size="5em" vertical-align="middle" />
-            </div>
-          </IconContext.Provider>
-        </section>
-      </section>
-      <section class="div">
-        <h2 className="heading">Any Additional Requests?</h2>
-        <Card class="container">
-          <CardContent class="content">
-            <Typography>Question 1</Typography>
-            <div className="icondiv">
-              <EditText
-                class="input"
-                labelText={"Enter Question Here"}
-                widthPercent={300}
-              />
-              <FaTrash />
-            </div>
-          </CardContent>
-          <CardContent>
+          <div>
             <p>
               Add more questions <MdAddCircle />
             </p>
-          </CardContent>
-        </Card>
-        <section className="section">
-          <IconContext.Provider value={{ className: "react-icons" }}>
-            <div>
-              <FaArrowDown size="5em" vertical-align="middle" />
-            </div>
-          </IconContext.Provider>
-        </section>
-      </section>
-      <section class="div">
-        <h2 className="heading">Service specifics</h2>
-        <Card class="container">
-          <CardContent className="sscontent">
-            <Typography>How long will the service take per request?</Typography>
-            <div className="icondiv">
-              <EditText
-                labelText={"0"}
-                widthPercent={50}
-                className="edittexts"
-              />
-              <p className="paragraphs">Hours</p>
-            </div>
-          </CardContent>
-          <CardContent className="sscontent">
-            <Typography>How many requests can you do at a time?</Typography>
-            <div className="icondiv">
-              <EditText labelText={"0"} widthPercent={50}></EditText>
-              <p className="paragraphs">Requests</p>
-            </div>
-          </CardContent>
-        </Card>
-        <section className="section">
-          <IconContext.Provider value={{ className: "react-icons" }}>
-            <div>
-              <FaArrowDown size="5em" vertical-align="middle" />
-            </div>
-          </IconContext.Provider>
-        </section>
-      </section>
-      <section class="div">
-        <h2 className="heading">How will you accept payments?</h2>
-        <Card class="container">
-          <CardContent class="content">
+          </div>
+        </div>
+        <div>
+          <h3>Service specifics</h3>
+        </div>
+        <div>
+          <Typography>How long will the service take per request?</Typography>
+          <div className="createproducticondiv">
+            <EditText
+              labelText={"0"}
+              widthPercent={50}
+              className="createproductedittexts"
+            />
+            <p className="createproductparagraphs">Hours</p>
+          </div>
+        </div>
+        <div>
+          <Typography>How many requests can you do at a time?</Typography>
+          <div className="createproducticondiv">
+            <EditText labelText={"0"} widthPercent={50}></EditText>
+            <p className="createproductparagraphs">Requests</p>
+          </div>
+        </div>
+        <div>
+          <h3>How will you accept payments?</h3>
+        </div>
+        <div className="buttonlistcontainer">
+          <div>
             <HelpButton fullWidth={false} label="Credit/Debit Card" />
-          </CardContent>
-          <CardContent class="content">
+          </div>
+          <div class="productbuttoncontainer">
             <HelpButton fullWidth={false} label="Cash" />
-          </CardContent>
-          <CardContent class="content">
+          </div>
+          <div class="productbuttoncontainer">
             <HelpButton fullWidth={false} label="Other" />
-          </CardContent>
-        </Card>
+          </div>
+        </div>
+        <div className="textmorespace">
+          <h3>
+            You're all set <FaThumbsUp />
+          </h3>
+        </div>
+        <div>
+          <HelpButton fullWidth={false} label="Create" />
+        </div>
+      </div>
+      {/*
+      
       </section>
-      <section className="footer">
-        <p>
-          You're all set <FaThumbsUp />
-        </p>
-        <HelpButton fullWidth={false} label="Create" />
-      </section>
-    </section>
+      
+  */}
+    </div>
   );
 }
