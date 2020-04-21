514d1bf532e9ec8a9582f8a8884900c74f85b8d3
shreshth kharbanda <shreshthkharbanda1@gmail.com>
2020-04-21 04:08:02 -0700

    Imported font awesome icons

--- WebsiteDev/src/App.js
+++ WebsiteDev/src/App.js
@@ -1,13 +1,18 @@
-import React, { Component } from "react";
-import { BrowserRouter } from "react-router-dom";
-import LandingPageNavigator from "./business/StartingScreens/LandingPage/LandingPageNavigator";
+import React, { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import LandingPageNavigator from './business/StartingScreens/LandingPage/LandingPageNavigator';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+
+library.add(fab, fas);
 
 export default class App extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <LandingPageNavigator/>
-      </BrowserRouter>
-    );
-  }
+	render() {
+		return (
+			<BrowserRouter>
+				<LandingPageNavigator />
+			</BrowserRouter>
+		);
+	}
 }
