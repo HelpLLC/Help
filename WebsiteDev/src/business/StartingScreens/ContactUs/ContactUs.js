c3289a3bf4b39ad71726c2ae2912fe7eb3977d21
shreshth kharbanda <shreshthkharbanda1@gmail.com>
2020-04-25 15:08:23 -0700

    Converted React Native to React JS

--- WebsiteDev/src/business/StartingScreens/ContactUs/ContactUs.js
+++ WebsiteDev/src/business/StartingScreens/ContactUs/ContactUs.js
@@ -1,20 +1,17 @@
 import React, { useState } from 'react';
-import { View, Image, Text, TouchableOpacity, Linking } from 'react-native-web';
+import './ContactUsStyle.css';
 import HelpButton from '../../../components/HelpButton/HelpButton';
 import HelpTextInput from '../../../components/HelpTextInput/HelpTextInput';
 import FirebaseFunctions from '../../../config/FirebaseFunctions';
+import strings from '../../../config/strings';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import ContactUsStyle from './ContactUsStyle';
+import path from './images/path_1.png';
+import map from './images/map.png';
 
 export default function ContactUs() {
 	const [email, setEmail] = useState('');
 	const [businessName, setBusinessName] = useState('');
 	const [message, setMessage] = useState('');
-
-	const sendEmail = () => {
-		const url = 'mailto:helpcocontact@gmail.com';
-		window.open(url, '_blank');
-	};
 
 	const loadInBrowser = (url) => {
 		window.open(url, '_blank');
@@ -22,53 +19,55 @@ export default function ContactUs() {
 
 	return (
 		<div className='contactUsContainer'>
-			<View style={ContactUsStyle.imageContainer}>
-				<Image style={ContactUsStyle.path} source={require('./images/path_1.png')} />
-				<View style={ContactUsStyle.textContainer}>
-					<Text style={ContactUsStyle.title}>Contact Us</Text>
-				</View>
-				<View style={ContactUsStyle.textContainer}>
-					<Text style={ContactUsStyle.explanation}>Let's launch your business into success!</Text>
-				</View>
-				<View style={ContactUsStyle.contactBorder}>
-					<View style={ContactUsStyle.heading1Container}>
-						<View style={ContactUsStyle.heading1}>Contact Information</View>
-
-						<View>
-							<View style={ContactUsStyle.rowContainer}>
-								<View style={ContactUsStyle.contactInfoIcon}>
+			<div className='imageContainer'>
+				<div className='path'>
+					<div className='textContainer'>
+						<text className='title'>{strings.ContactUs}</text>
+					</div>
+					<div className='textContainer'>
+						<text className='explanation'>{strings.LetsLaunch}</text>
+					</div>
+				</div>
+				<div className='contactBorder'>
+					<div className='heading1Container'>
+						<div className='heading1'>{strings.ContactInformation}</div>
+						<div>
+							<div className='rowContainer'>
+								<div className='contactInfoIcon'>
 									<FontAwesomeIcon icon={['fas', 'map-marker-alt']} size='2x' />
-								</View>
-								<Text style={ContactUsStyle.contactInfoText}>
-									13543 NE 200th ST Woodinville, WA 98072
-								</Text>
-							</View>
-							<View style={ContactUsStyle.rowContainer}>
-								<View style={ContactUsStyle.contactInfoIcon}>
+								</div>
+								<a
+									className='contactInfoText'
+									href='https://www.google.com/maps/place/13543+NE+200th+St,+Woodinville,+WA+98072/@47.7726099,-122.1614489,17z/data=!3m1!4b1!4m5!3m4!1s0x54900e94272d1a0f:0x381c31df309db56d!8m2!3d47.7726063!4d-122.1592602'
+									target='_blank'>
+									{strings.HelpAddress}
+								</a>
+							</div>
+							<div className='rowContainer'>
+								<div className='contactInfoIcon'>
 									<FontAwesomeIcon icon={['fas', 'envelope-square']} size='2x' />
-								</View>
-								<TouchableOpacity style={ContactUsStyle.contactInfoText} onPress={() => sendEmail}>
-									helpcocontact@gmail.com
-								</TouchableOpacity>
-							</View>
-							<View style={ContactUsStyle.rowContainer}>
-								<View style={ContactUsStyle.contactInfoIcon}>
+								</div>
+								<a
+									className='contactInfoText'
+									href='mailto:helpcocontact@gmail.com'
+									target='_blank'>
+									{strings.HelpEmail}
+								</a>
+							</div>
+							<div className='rowContainer'>
+								<div className='contactInfoIcon'>
 									<FontAwesomeIcon icon={['fas', 'phone']} size='2x' />
-								</View>
-								<TouchableOpacity
-									style={ContactUsStyle.contactInfoText}
-									onPress={() => {
-										Linking.openURL('tel:4252299185');
-									}}>
-									+1 (425) 229-9185{' '}
-								</TouchableOpacity>
-							</View>
-						</View>
-					</View>
+								</div>
+								<a className='contactInfoText' href='tel:4252299185' target='_blank'>
+									{strings.HelpPhoneNumber}
+								</a>
+							</div>
+						</div>
+					</div>
 
-					<View style={ContactUsStyle.imageContainer}>
-						<Image source={require('./images/map.png')} style={ContactUsStyle.mapImage} />
-						<View style={ContactUsStyle.textContainer}>
+					<div className='imageContainer'>
+						<img src={map} className='mapImage' />
+						<div className='textContainer'>
 							<HelpButton
 								width={'15vw'}
 								title='Go To Map'
@@ -78,19 +77,19 @@ export default function ContactUs() {
 									)
 								}
 							/>
-						</View>
-					</View>
-				</View>
+						</div>
+					</div>
+				</div>
 
-				<View style={ContactUsStyle.heading2Container}>
-					<Text style={ContactUsStyle.heading2}>Let's Grow Your Business</Text>
-					<Text style={ContactUsStyle.subtitle2}>
-						Ready to start working together? Have a general question? We're only an email away.
-					</Text>
-				</View>
-			</View>
-			<View style={ContactUsStyle.formContainer}>
-				<View style={ContactUsStyle.emailNameContainer}>
+				<div className='heading2Container'>
+					<text className='heading2'>{strings.LetsGrow}</text>
+					<text className='subtitle2'>
+						{strings.ReadyToStartWorkingTogether}
+					</text>
+				</div>
+			</div>
+			<div className='formContainer'>
+				<div className='emailNameContainer'>
 					<HelpTextInput
 						height={'7vh'}
 						width={'45vw'}
@@ -105,7 +104,7 @@ export default function ContactUs() {
 						isMultiline={false}
 						onChangeText={(name) => setBusinessName(name)}
 					/>
-				</View>
+				</div>
 
 				<HelpTextInput
 					height={'35vh'}
@@ -114,20 +113,21 @@ export default function ContactUs() {
 					isMultiline={true}
 					onChangeText={(message) => setMessage(message)}
 				/>
-			</View>
-			<View style={ContactUsStyle.buttonContainer}>
+			</div>
+			<div className='buttonContainer'>
 				<HelpButton
 					title='SUBMIT'
 					width={'15vw'}
-					onPress={async () =>
+					onPress={async () => {
 						await FirebaseFunctions.call('sendEmail', {
-							recepient: 'helpcocontact@gmail.com',
-							subject: 'Contact Us Form from ' + businessName,
-							text: message,
-						})
-					}
+							recepient: strings.HelpEmail,
+							subject:
+								businessName !== '' ? 'Contact Us Form from ' + businessName : 'Contact Us Form',
+							text: message + '\nReply to: ' + email,
+						});
+					}}
 				/>
-			</View>
+			</div>
 		</div>
 	);
 }
