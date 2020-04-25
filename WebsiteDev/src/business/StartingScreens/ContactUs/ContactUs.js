ad2cb789cb390874ec1ad3da0fc280531835f5a5
shreshth kharbanda <shreshthkharbanda1@gmail.com>
2020-04-25 12:45:41 -0700

    Refactored Contact Us
    Converted inline styles to a stylesheet in a new file. Converted perchanges to viewport heights and widths.

--- WebsiteDev/src/business/StartingScreens/ContactUs/ContactUs.js
+++ WebsiteDev/src/business/StartingScreens/ContactUs/ContactUs.js
@@ -1,13 +1,10 @@
 import React, { useState } from 'react';
-// import './ContactUs.css';
 import { View, Image, Text, TouchableOpacity, Linking } from 'react-native-web';
 import HelpButton from '../../../components/HelpButton/HelpButton';
 import HelpTextInput from '../../../components/HelpTextInput/HelpTextInput';
 import FirebaseFunctions from '../../../config/FirebaseFunctions';
-import colors from '../../../config/colors';
-import { screenHeight, screenWidth } from '../../../config/dimensions';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import './ContactUsStyle.css';
+import ContactUsStyle from './ContactUsStyle';
 
 export default function ContactUs() {
 	const [email, setEmail] = useState('');
@@ -25,151 +22,41 @@ export default function ContactUs() {
 
 	return (
 		<div className='contactUsContainer'>
-			<View
-				style={{
-					alignItems: 'center',
-				}}>
-				<Image
-					style={{
-						position: 'absolute',
-						top: 0,
-						left: 0,
-						bottom: 0,
-						right: 0,
-						height: '80vh',
-						opacity: 1,
-					}}
-					source={require('./images/path_1.png')}
-				/>
-				<View
-					style={{
-						marginTop: '5%',
-					}}>
-					<Text
-						style={{
-							fontFamily: 'Lucida Grande',
-							fontSize: '87px',
-							textAlign: 'left',
-							color: '#ffffff',
-						}}>
-						Contact Us
-					</Text>
+			<View style={ContactUsStyle.imageContainer}>
+				<Image style={ContactUsStyle.path} source={require('./images/path_1.png')} />
+				<View style={ContactUsStyle.textContainer}>
+					<Text style={ContactUsStyle.title}>Contact Us</Text>
 				</View>
-				<View
-					style={{
-						marginTop: '9vh',
-					}}>
-					<Text
-						style={{
-							overflow: 'hidden',
-							fontFamily: 'Lucida Grande',
-							fontSize: '130px',
-							textAlign: 'center',
-							lineHeight: '130px',
-							color: '#ffffff',
-						}}>
-						Let's launch your business into success!
-					</Text>
+				<View style={ContactUsStyle.textContainer}>
+					<Text style={ContactUsStyle.explanation}>Let's launch your business into success!</Text>
 				</View>
-				<View
-					style={{
-						marginTop: '30vh',
-						borderRadius: '25px',
-						border: '2px solid #5cc6bc',
-						boxShadow: '0px 3px 5px 1px rgba(92, 198, 188, 2.55)',
-						flexDirection: 'row',
-						paddingHorizontal: '2vw',
-						paddingVertical: '5vw',
-					}}>
-					<View
-						style={{
-							flexDirection: 'column',
-							justifyContent: 'center',
-						}}>
-						<View
-							style={{
-								fontFamily: 'Lucida Grande',
-								fontSize: '56px',
-								textAlign: 'left',
-								color: '#567681',
-							}}>
-							Contact Information
-						</View>
+				<View style={ContactUsStyle.contactBorder}>
+					<View style={ContactUsStyle.heading1Container}>
+						<View style={ContactUsStyle.heading1}>Contact Information</View>
 
 						<View>
-							<View style={{ flexDirection: 'row' }}>
-								<FontAwesomeIcon
-									icon={['fas', 'map-marker-alt']}
-									style={{
-										overflow: 'hidden',
-										fontFamily: 'Lucida Grande',
-										fontSize: '35px',
-										textAlign: 'left',
-										color: '#000000',
-										marginTop: '2.5%',
-									}}
-								/>
-								<Text
-									style={{
-										overflow: 'hidden',
-										fontFamily: 'Lucida Grande',
-										fontSize: '40px',
-										textAlign: 'left',
-										color: '#567681',
-										marginTop: '2%',
-										marginLeft: '2%',
-									}}>
+							<View style={ContactUsStyle.rowContainer}>
+								<View style={ContactUsStyle.contactInfoIcon}>
+									<FontAwesomeIcon icon={['fas', 'map-marker-alt']} size='2x' />
+								</View>
+								<Text style={ContactUsStyle.contactInfoText}>
 									13543 NE 200th ST Woodinville, WA 98072
 								</Text>
 							</View>
-							<View style={{ flexDirection: 'row' }}>
-								<FontAwesomeIcon
-									icon={['fas', 'envelope-square']}
-									style={{
-										overflow: 'hidden',
-										fontFamily: 'Lucida Grande',
-										fontSize: '40px',
-										textAlign: 'left',
-										color: '#000000',
-										marginTop: '2.5%',
-									}}
-								/>
-								<TouchableOpacity
-									style={{
-										overflow: 'hidden',
-										fontFamily: 'Lucida Grande',
-										fontSize: '40px',
-										textAlign: 'left',
-										color: '#567681',
-										marginTop: '2%',
-										marginLeft: '2%',
-									}}
-									onPress={() => sendEmail}>
+							<View style={ContactUsStyle.rowContainer}>
+								<View style={ContactUsStyle.contactInfoIcon}>
+									<FontAwesomeIcon icon={['fas', 'envelope-square']} size='2x' />
+								</View>
+								<TouchableOpacity style={ContactUsStyle.contactInfoText} onPress={() => sendEmail}>
 									helpcocontact@gmail.com
 								</TouchableOpacity>
 							</View>
-							<View style={{ flexDirection: 'row' }}>
-								<FontAwesomeIcon
-									icon={['fas', 'phone']}
-									style={{
-										overflow: 'hidden',
-										fontFamily: 'Lucida Grande',
-										fontSize: '35px',
-										textAlign: 'left',
-										color: '#000000',
-										marginTop: '3%',
-									}}
-								/>
+							<View style={ContactUsStyle.rowContainer}>
+								<View style={ContactUsStyle.contactInfoIcon}>
+									<FontAwesomeIcon icon={['fas', 'phone']} size='2x' />
+								</View>
 								<TouchableOpacity
-									style={{
-										overflow: 'hidden',
-										fontFamily: 'Lucida Grande',
-										fontSize: '40px',
-										textAlign: 'left',
-										color: '#567681',
-										marginTop: '2%',
-										marginLeft: '2%',
-									}}
+									style={ContactUsStyle.contactInfoText}
 									onPress={() => {
 										Linking.openURL('tel:4252299185');
 									}}>
@@ -179,15 +66,9 @@ export default function ContactUs() {
 						</View>
 					</View>
 
-					<View style={{ alignItems: 'center' }}>
-						<Image
-							source={require('./images/map.png')}
-							style={{ width: '30vw', height: '30vw' }}
-						/>
-						<View
-							style={{
-								marginTop: '5%',
-							}}>
+					<View style={ContactUsStyle.imageContainer}>
+						<Image source={require('./images/map.png')} style={ContactUsStyle.mapImage} />
+						<View style={ContactUsStyle.textContainer}>
 							<HelpButton
 								width={'15vw'}
 								title='Go To Map'
@@ -201,75 +82,40 @@ export default function ContactUs() {
 					</View>
 				</View>
 
-				<View
-					style={{
-						justifyContent: 'flex-start',
-						alignItems: 'center',
-						flexDirection: 'column',
-						width: '90vw',
-						marginTop: '15vh',
-					}}>
-					<Text
-						style={{
-							fontFamily: 'Lucida Grande',
-							fontSize: '79px',
-							textAlign: 'left',
-							color: '#567681',
-						}}>
-						Let's Grow Your Business
-					</Text>
-					<Text
-						style={{
-							overflow: 'hidden',
-							fontFamily: 'Lucida Grande',
-							fontSize: '50px',
-							textAlign: 'center',
-							lineHeight: '7.5vh',
-							color: '#00b0f0',
-							marginVertical: '4vh',
-						}}>
-						Ready to start working together? Have a general question? We're only an
-						email away.
+				<View style={ContactUsStyle.heading2Container}>
+					<Text style={ContactUsStyle.heading2}>Let's Grow Your Business</Text>
+					<Text style={ContactUsStyle.subtitle2}>
+						Ready to start working together? Have a general question? We're only an email away.
 					</Text>
 				</View>
 			</View>
-			<View
-				style={{
-					flexDirection: 'row',
-					width: '95vw',
-					marginBottom: '5vh',
-					alignItems: 'center',
-					justifyContent: 'space-between',
-				}}>
+			<View style={ContactUsStyle.formContainer}>
+				<View style={ContactUsStyle.emailNameContainer}>
+					<HelpTextInput
+						height={'7vh'}
+						width={'45vw'}
+						placeholder='E-mail Address'
+						isMultiline={false}
+						onChangeText={(email) => setEmail(email)}
+					/>
+					<HelpTextInput
+						height='7vh'
+						width={'45vw'}
+						placeholder='Business Name (Optional)'
+						isMultiline={false}
+						onChangeText={(name) => setBusinessName(name)}
+					/>
+				</View>
+
 				<HelpTextInput
-					height={'7vh'}
-					width={'45vw'}
-					placeholder='E-mail Address'
-					isMultiline={false}
-					onChangeText={(email) => setEmail(email)}
-				/>
-				<HelpTextInput
-					height='7vh'
-					width={'45vw'}
-					placeholder='Business Name (Optional)'
-					isMultiline={false}
-					onChangeText={(name) => setBusinessName(name)}
+					height={'35vh'}
+					width={'95vw'}
+					placeholder='Message'
+					isMultiline={true}
+					onChangeText={(message) => setMessage(message)}
 				/>
 			</View>
-			<HelpTextInput
-				height={'35vh'}
-				width={'95vw'}
-				placeholder='Message'
-				isMultiline={true}
-				onChangeText={(message) => setMessage(message)}></HelpTextInput>
-			<View
-				style={{
-					marginTop: '2vh',
-					marginBottom: '3vh',
-					alignItems: 'flex-end',
-					width: '95vw',
-					alignSelf: 'center',
-				}}>
+			<View style={ContactUsStyle.buttonContainer}>
 				<HelpButton
 					title='SUBMIT'
 					width={'15vw'}
