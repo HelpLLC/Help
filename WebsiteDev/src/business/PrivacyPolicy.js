7a1bcb344c8a281088b6a47d383fcb57f9632b39
Zyad <zyadgohary@hotmail.com>
2020-04-25 16:14:15 -0700

    Added designs

--- WebsiteDev/src/business/PrivacyPolicy.js
+++ WebsiteDev/src/business/PrivacyPolicy.js
@@ -4,110 +4,275 @@ import TitleComponent from '../components/TitleComponent';
 export default class PrivacyPolicy extends Component {
 	render() {
 		return (
-         <div>
-            <TitleComponent text={'Privacy Policy'} isCentered={true} backgroundColor={'inherit'} textColor={"#00B0F0"} fontSize={50}/>
+			<div>
+				<TitleComponent
+					text={'Privacy Policy'}
+					isCentered={true}
+					backgroundColor={'inherit'}
+					textColor={'#00B0F0'}
+					fontSize={50}
+				/>
 
-            <TitleComponent text={'Welcome to our Privacy Policy'} isCentered={false} backgroundColor={'inherit'} fontSize={14} />
-            <TitleComponent text={'--Your privacy is critically important to us.'} isCentered={false} backgroundColor={'inherit'} fontSize={14} />
-            
-            <TitleComponent text={'Help is located at: Help LLC 13543 NE 200th St Washington, United States'} isCentered={false} backgroundColor={'inherit'} fontSize={14} />
-            <TitleComponent text={'Help can be contacted at: (425) 229-9185'} isCentered={false} backgroundColor={'inherit'} fontSize={14} />
-            <TitleComponent text={'It is Help LLC\'s policy to respect your privacy regarding any information we may' +
-            ' collect while operating our website. This Privacy Policy applies to https://helpcocontact.wixsite.com/website' +
-            ' (hereinafter, "us", "we", or "https://helpcocontact.wixsite.com/website"). We respect your privacy and are committed to' +
-            ' protecting personally identifiable information you may provide us through the Website. We have adopted this privacy policy' +
-            ' ("Privacy Policy") to explain what information may be collected on our Website, how we use this information, and under what' +
+				<TitleComponent
+					text={'Welcome to our Privacy Policy'}
+					isCentered={false}
+					backgroundColor={'inherit'}
+					fontSize={14}
+				/>
+				<TitleComponent
+					text={'--Your privacy is critically important to us.'}
+					isCentered={false}
+					backgroundColor={'inherit'}
+					fontSize={14}
+				/>
+				<TitleComponent
+					text={'Help is located at: Help LLC 13543 NE 200th St Washington, United States'}
+					isCentered={false}
+					backgroundColor={'inherit'}
+					fontSize={14}
+				/>
+				<TitleComponent
+					text={'Help can be contacted at: (425) 229-9185'}
+					isCentered={false}
+					backgroundColor={'inherit'}
+					fontSize={14}
+				/>
+				<TitleComponent
+					text={
+						"It is Help LLC's policy to respect your privacy regarding any information we may" +
+						' collect while operating our website. This Privacy Policy applies to https://helpcocontact.wixsite.com/website' +
+						' (hereinafter, "us", "we", or "https://helpcocontact.wixsite.com/website"). We respect your privacy and are committed to' +
+						' protecting personally identifiable information you may provide us through the Website. We have adopted this privacy policy' +
+						' ("Privacy Policy") to explain what information may be collected on our Website, how we use this information, and under what' +
 						' circumstances we may disclose the information to third parties. This Privacy Policy' +
 						' applies only to information we collect through the Website and does not apply to our' +
 						' collection of information from other sources. This Privacy Policy, together with the' +
 						' Terms and conditions posted on our Website, set forth the general rules and policies' +
 						' governing your use of our Website. Depending on your activities when visiting our' +
-						' Website, you may be required to agree to additional terms and conditions.'} isCentered={false} backgroundColor={'inherit'} fontSize={14} />
+						' Website, you may be required to agree to additional terms and conditions.'
+					}
+					isCentered={false}
+					backgroundColor={'inherit'}
+					fontSize={14}
+				/>
 
-<TitleComponent text={'Website Visitors'} isCentered={true} textColor={"#00B0F0"} backgroundColor={'inherit'} fontSize={18} />
-<TitleComponent text={' Like most website operators, Help LLC collects' +
+				<TitleComponent
+					text={'Website Visitors'}
+					isCentered={true}
+					textColor={'#00B0F0'}
+					backgroundColor={'inherit'}
+					fontSize={18}
+				/>
+				<TitleComponent
+					text={
+						' Like most website operators, Help LLC collects' +
 						' non-personally-identifying information of the sort that web browsers and servers' +
 						' typically make available, such as the browser type, language preference, referring site,' +
-						' and the date and time of each visitor request. Help LLC\'s purpose in collecting non-' +
-						' personally identifying information is to better understand how Help LL\'s visitors use' +
+						" and the date and time of each visitor request. Help LLC's purpose in collecting non-" +
+						" personally identifying information is to better understand how Help LL's visitors use" +
 						' its website. From time to time, Help LLC may release non-personally-identifying' +
 						' information in the aggregate, e.g., by publishing a report on trends in the usage of its' +
 						' website. Help LLC also collects potentially personally identifying information like' +
 						' Internet Protocol(IP) addresses for logged in users and for users leaving comments on' +
 						' https://helpcocontact.wixsite.com/website blog posts. Help LLC only discloses logged in' +
 						' user and commenter IP addresses under the same circumstances that it uses and discloses' +
-                  ' personally-identifying information as described below.'} isCentered={false} backgroundColor={'inherit'} fontSize={14} />
+						' personally-identifying information as described below.'
+					}
+					isCentered={false}
+					backgroundColor={'inherit'}
+					fontSize={14}
+				/>
 
-
-<TitleComponent text={'Gathering of Personally-Identifying Information'} textColor={"#00B0F0"} isCentered={true} backgroundColor={'inherit'} fontSize={18} />
-<TitleComponent text={' Certain visitors to Help LLC\'s' +
+				<TitleComponent
+					text={'Gathering of Personally-Identifying Information'}
+					textColor={'#00B0F0'}
+					isCentered={true}
+					backgroundColor={'inherit'}
+					fontSize={18}
+				/>
+				<TitleComponent
+					text={
+						" Certain visitors to Help LLC's" +
 						' websites choose to interact with Help LLC in ways that require Help LLC to gather' +
 						' personally-identifying information. The amount and type of information that Help LLC' +
 						' gathers depends on the nature of the interaction. For example, we ask visitors who sign' +
 						' up for a blog at https://helpcocontact.wixsite.com/website to provide a username and' +
-						' email address.'} isCentered={false} backgroundColor={'inherit'} fontSize={14} />
+						' email address.'
+					}
+					isCentered={false}
+					backgroundColor={'inherit'}
+					fontSize={14}
+				/>
 
+				<TitleComponent
+					text={'License'}
+					isCentered={true}
+					backgroundColor={'inherit'}
+					textColor={'#00B0F0'}
+					fontSize={40}
+				/>
 
-                  <TitleComponent text={'License'} isCentered={true} backgroundColor={'inherit'} textColor={"#00B0F0"} fontSize={40} />
-
-                  <TitleComponent text={'Security'} isCentered={true} textColor={"#00B0F0"} backgroundColor={'inherit'} fontSize={18} />
-<TitleComponent text={'The security of your Personal Information is important to us, but remember' +
+				<TitleComponent
+					text={'Security'}
+					isCentered={true}
+					textColor={'#00B0F0'}
+					backgroundColor={'inherit'}
+					fontSize={18}
+				/>
+				<TitleComponent
+					text={
+						'The security of your Personal Information is important to us, but remember' +
 						' that no method of transmission over the Internet, or method of electronic storage is 100' +
 						' % secure. While we strive to use commercially acceptable means to protect your Personal' +
-						' Information, we cannot guarantee its absolute security.'} isCentered={false} backgroundColor={'inherit'} fontSize={14} />
+						' Information, we cannot guarantee its absolute security.'
+					}
+					isCentered={false}
+					backgroundColor={'inherit'}
+					fontSize={14}
+				/>
 
-<TitleComponent text={'Advertisements'} isCentered={true} textColor={"#00B0F0"} backgroundColor={'inherit'} fontSize={18} />
-                  <TitleComponent text={'Ads appearing on our website may be delivered to users by advertising' +
+				<TitleComponent
+					text={'Advertisements'}
+					isCentered={true}
+					textColor={'#00B0F0'}
+					backgroundColor={'inherit'}
+					fontSize={18}
+				/>
+				<TitleComponent
+					text={
+						'Ads appearing on our website may be delivered to users by advertising' +
 						' partners, who may set cookies. These cookies allow the ad server to recognize your' +
 						' computer each time they send you an online advertisement to compile information about' +
 						' you or others who use your computer. This information allows ad networks to, among other' +
 						' things, deliver targeted advertisements that they believe will be of most interest to' +
 						' you. This Privacy Policy covers the use of cookies by Help LLC and does not cover the' +
-						' use of cookies by any advertisers.'} isCentered={false} backgroundColor={'inherit'} fontSize={14} />
+						' use of cookies by any advertisers.'
+					}
+					isCentered={false}
+					backgroundColor={'inherit'}
+					fontSize={14}
+				/>
 
-<TitleComponent text={'Links To External Sites'} textColor={"#00B0F0"} isCentered={true} backgroundColor={'inherit'} fontSize={18} />
-                                    <TitleComponent text={' Our Service may contain links to external sites that are not' +
+				<TitleComponent
+					text={'Links To External Sites'}
+					textColor={'#00B0F0'}
+					isCentered={true}
+					backgroundColor={'inherit'}
+					fontSize={18}
+				/>
+				<TitleComponent
+					text={
+						' Our Service may contain links to external sites that are not' +
 						' operated by us. If you click on a third party link, you will be directed to that third' +
-						' party\'s site. We strongly advise you to review the Privacy Policy and terms and' +
-						' conditions of every site you visit. \'We have no control over, and assume no' +
+						" party's site. We strongly advise you to review the Privacy Policy and terms and" +
+						" conditions of every site you visit. 'We have no control over, and assume no" +
 						' responsibility for the content, privacy policies or practices of any third party sites,' +
-						' products or services.'} isCentered={false} backgroundColor={'inherit'} fontSize={14} />
+						' products or services.'
+					}
+					isCentered={false}
+					backgroundColor={'inherit'}
+					fontSize={14}
+				/>
 
-<TitleComponent text={'Aggregated Statistics'} textColor={"#00B0F0"} isCentered={true} backgroundColor={'inherit'} fontSize={18} />
-                                                      <TitleComponent text={' \'Help LLC may collect statistics about the behavior of visitors' +
+				<TitleComponent
+					text={'Aggregated Statistics'}
+					textColor={'#00B0F0'}
+					isCentered={true}
+					backgroundColor={'inherit'}
+					fontSize={18}
+				/>
+				<TitleComponent
+					text={
+						" 'Help LLC may collect statistics about the behavior of visitors" +
 						' to its website.Help LLC may display this information publicly or provide it to others.' +
-						' However, Help LLC does not disclose your personally identifying information.'} isCentered={false} backgroundColor={'inherit'} fontSize={14} />
+						' However, Help LLC does not disclose your personally identifying information.'
+					}
+					isCentered={false}
+					backgroundColor={'inherit'}
+					fontSize={14}
+				/>
 
-<TitleComponent text={'Cookies'} isCentered={true} textColor={"#00B0F0"} backgroundColor={'inherit'} fontSize={18} />
-                                                                        <TitleComponent text={' To enrich and perfect your online experience, Help LLC uses "Cookies", similar' +
+				<TitleComponent
+					text={'Cookies'}
+					isCentered={true}
+					textColor={'#00B0F0'}
+					backgroundColor={'inherit'}
+					fontSize={18}
+				/>
+				<TitleComponent
+					text={
+						' To enrich and perfect your online experience, Help LLC uses "Cookies", similar' +
 						' technologies and services provided by others to display personalized content,' +
 						' appropriate advertising and store your preferences on your computer. A cookie is a' +
-						' string of information that a website stores on a visitor\'s computer, and that the' +
-						' visitor\'s browser provides to the website each time the visitor returns .Help LLC uses' +
+						" string of information that a website stores on a visitor's computer, and that the" +
+						" visitor's browser provides to the website each time the visitor returns .Help LLC uses" +
 						' cookies to help Help LLC identify and track visitors, their usage of' +
 						' https://helpcocontact.wixsite.com/website, and their website access preferences. Help' +
 						' LLC visitors who do not wish to have cookies placed on their computers should set their' +
-						' browsers to refuse cookies before using Help LLC\'s websites, with the drawback that' +
-						' certain features of Help LLC\'s websites may not function properly without the aid of' +
+						" browsers to refuse cookies before using Help LLC's websites, with the drawback that" +
+						" certain features of Help LLC's websites may not function properly without the aid of" +
 						' cookies. By continuing to navigate our website without changing your cookie settings,' +
-                  ' you hereby acknowledge and agree to Help LLC\'s use of cookies.'} isCentered={false} backgroundColor={'inherit'} fontSize={14} />
-                  
-<TitleComponent text={'Privacy Policy Changes'} textColor={"#00B0F0"} isCentered={true} backgroundColor={'inherit'} fontSize={18} />
-                  <TitleComponent text={' Although most changes are likely to be minor, Help LLC may' +
-                     ' change its Privacy Policy from time to time, and in Help LLC\'s sole discretion. Help LLC' +
-                     ' encourages visitors to frequently check this page for any changes to its Privacy Policy.' +
-                     ' Your continued use of this site after any change in this Privacy Policy will constitute' +
-                     ' your acceptance of such change.'} isCentered={false} backgroundColor={'inherit'} fontSize={14} />
-                  
-<TitleComponent text={'Email Addresses '} textColor={"#00B0F0"} isCentered={true} backgroundColor={'inherit'} fontSize={18} />
-                     <TitleComponent text={' Collected email addresses in order to create account information from' +
-                        ' customers.'} isCentered={false} backgroundColor={'inherit'} fontSize={14} />
-                  
-<TitleComponent text={'Credit and Contact Information '} isCentered={true} textColor={"#00B0F0"} backgroundColor={'inherit'} fontSize={18} />
-                        <TitleComponent text={' This privacy policy was created at' +
-                           ' https://termsandconditionstemplate.com/privacy-policy-generator. If you have any' +
-                           ' questions about this Privacy Policy, please contact us via email or phone.'} isCentered={false} backgroundColor={'inherit'} fontSize={14} />
-         </div>
-      );
+						" you hereby acknowledge and agree to Help LLC's use of cookies."
+					}
+					isCentered={false}
+					backgroundColor={'inherit'}
+					fontSize={14}
+				/>
+
+				<TitleComponent
+					text={'Privacy Policy Changes'}
+					textColor={'#00B0F0'}
+					isCentered={true}
+					backgroundColor={'inherit'}
+					fontSize={18}
+				/>
+				<TitleComponent
+					text={
+						' Although most changes are likely to be minor, Help LLC may' +
+						" change its Privacy Policy from time to time, and in Help LLC's sole discretion. Help LLC" +
+						' encourages visitors to frequently check this page for any changes to its Privacy Policy.' +
+						' Your continued use of this site after any change in this Privacy Policy will constitute' +
+						' your acceptance of such change.'
+					}
+					isCentered={false}
+					backgroundColor={'inherit'}
+					fontSize={14}
+				/>
+
+				<TitleComponent
+					text={'Email Addresses '}
+					textColor={'#00B0F0'}
+					isCentered={true}
+					backgroundColor={'inherit'}
+					fontSize={18}
+				/>
+				<TitleComponent
+					text={
+						' Collected email addresses in order to create account information from' + ' customers.'
+					}
+					isCentered={false}
+					backgroundColor={'inherit'}
+					fontSize={14}
+				/>
+
+				<TitleComponent
+					text={'Credit and Contact Information '}
+					isCentered={true}
+					textColor={'#00B0F0'}
+					backgroundColor={'inherit'}
+					fontSize={18}
+				/>
+				<TitleComponent
+					text={
+						' This privacy policy was created at' +
+						' https://termsandconditionstemplate.com/privacy-policy-generator. If you have any' +
+						' questions about this Privacy Policy, please contact us via email or phone.'
+					}
+					isCentered={false}
+					backgroundColor={'inherit'}
+					fontSize={14}
+				/>
+			</div>
+		);
 	}
 }
--- WebsiteDev/src/components/TimePicker.js
+++ WebsiteDev/src/components/TimePicker.js
@@ -1,54 +1,50 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import TimeInput from 'material-ui-time-picker';
-import {
-   createMuiTheme,
-   ThemeProvider
- } from '@material-ui/core/styles';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 
- class TimePicker extends Component {
-render() {
+class TimePicker extends Component {
+	render() {
+		const { widthPercent, marginLeft, marginTop } = this.props;
 
-   const { widthPercent, marginLeft, marginTop } = this.props;
+		const theme = createMuiTheme({
+			palette: {
+				primary: {
+					main: '#00B0F0',
+				},
+			},
+			textAlign: 'center',
+			color: '#00B0F0',
+		});
 
-   const theme = createMuiTheme({
-      palette: {
-        primary: {
-          main: '#00B0F0'
-        }
-      },
-      textAlign: "center",
-      color: '#00B0F0'
-    });
+		const mystyle = {
+			color: '#00B0F0',
+			fontFamily: 'Arial Rounded MT Bold',
+			marginLeft: this.props.marginLeft,
+			fontSize: 14,
+			width: this.props.widthPercent,
+			marginTop: this.props.marginTop,
+		};
 
-   const mystyle = {
-      color: "#00B0F0",
-      fontFamily: "Arial Rounded MT Bold",
-      marginLeft: this.props.marginLeft,
-      fontSize: 14,
-      width: this.props.widthPercent,
-      marginTop: this.props.marginTop,
-    };
-
-  return (
-    <div>
-       <ThemeProvider theme={theme}>
-            <TimeInput
-               mode='12h'
-               // style={mystyle}
-               style ={mystyle}
-               onChange={this.props.onChange}
-               value={this.props.value}
-            />
-         </ThemeProvider>
-    </div>
-  );
-}
-propTypes = {
-   widthPercent: PropTypes.string.isRequired,
-   marginLeft: PropTypes.number.isRequired,
-   marginTop: PropTypes.number.isRequired
- };
+		return (
+			<div>
+				<ThemeProvider theme={theme}>
+					<TimeInput
+						mode='12h'
+						// style={mystyle}
+						style={mystyle}
+						onChange={this.props.onChange}
+						value={this.props.value}
+					/>
+				</ThemeProvider>
+			</div>
+		);
+	}
+	propTypes = {
+		widthPercent: PropTypes.string.isRequired,
+		marginLeft: PropTypes.number.isRequired,
+		marginTop: PropTypes.number.isRequired,
+	};
 }
 
 //exports the module
