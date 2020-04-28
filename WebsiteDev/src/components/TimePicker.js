99b2792fd8cad7d536b1834fb2d5b265fc065ed1
shreshth kharbanda <shreshthkharbanda1@gmail.com>
2020-04-28 13:38:00 -0700

    Changed TimePicker to functional component and react native

--- WebsiteDev/src/components/TimePicker.js
+++ WebsiteDev/src/components/TimePicker.js
@@ -1,55 +1,52 @@
-import React, { Component } from "react";
+import React from 'react';
 import PropTypes from 'prop-types';
 import TimeInput from 'material-ui-time-picker';
-import {
-   createMuiTheme,
-   ThemeProvider
- } from '@material-ui/core/styles';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import { View } from 'react-native-web';
 
- class TimePicker extends Component {
-render() {
+export default function TimePicker(props) {
+  
+	TimePicker.propTypes = {
+		widthPercent: PropTypes.string.isRequired,
+		marginLeft: PropTypes.number.isRequired,
+		marginTop: PropTypes.number.isRequired,
+  };
+  
+  const { 
+    widthPercent, 
+    marginLeft, 
+    marginTop 
+  } = props;
 
-   const { widthPercent, marginLeft, marginTop } = this.props;
+		const theme = createMuiTheme({
+			palette: {
+				primary: {
+					main: '#00B0F0',
+				},
+			},
+			textAlign: 'center',
+			color: '#00B0F0',
+		});
 
-   const theme = createMuiTheme({
-      palette: {
-        primary: {
-          main: '#00B0F0'
-        }
-      },
-      textAlign: "center",
-      color: '#00B0F0'
-    });
+		const mystyle = {
+			color: '#00B0F0',
+			fontFamily: 'Arial Rounded MT Bold',
+			marginLeft: props.marginLeft,
+			fontSize: 14,
+			width: props.widthPercent,
+			marginTop: props.marginTop,
+		};
 
-   const mystyle = {
-      color: "#00B0F0",
-      fontFamily: "Arial Rounded MT Bold",
-      marginLeft: this.props.marginLeft,
-      fontSize: 14,
-      width: this.props.widthPercent,
-      marginTop: this.props.marginTop,
-    };
-
-  return (
-    <div>
-       <ThemeProvider theme={theme}>
-            <TimeInput
-               mode='12h'
-               // style={mystyle}
-               style ={mystyle}
-               onChange={this.props.onChange}
-               value={this.props.value}
-            />
-         </ThemeProvider>
-    </div>
-  );
+		return (
+			<View>
+				<ThemeProvider theme={theme}>
+					<TimeInput
+						mode='12h'
+						style={mystyle}
+						onChange={props.onChange}
+						value={props.value}
+					/>
+				</ThemeProvider>
+			</View>
+		);
 }
-propTypes = {
-   widthPercent: PropTypes.string.isRequired,
-   marginLeft: PropTypes.number.isRequired,
-   marginTop: PropTypes.number.isRequired
- };
-}
-
-//exports the module
-export default TimePicker;
