bbc3b53a1a91979d495e6ee82c8c1ca3e1be1164
khalidelho <47620535+khalidelho@users.noreply.github.com>
2020-05-21 17:13:20 -0700

    Calendar.js

--- WebsiteDev/src/App.js
+++ WebsiteDev/src/App.js
@@ -4,14 +4,16 @@ import LandingPageNavigator from './business/StartingScreens/LandingPage/Landing
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 import { fas } from '@fortawesome/free-solid-svg-icons';
-import ReactCalendar from './business/BusinessScreens/Calender/Calender'
+
 
 library.add(fab, fas);
 
 export default class App extends Component {
 	render() {
 		return (
-			<ReactCalendar/>
+			<BrowserRouter>
+			<LandingPageNavigator />
+		</BrowserRouter>
 		);
 	}
 }
