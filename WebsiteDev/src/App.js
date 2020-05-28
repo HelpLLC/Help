d2abad29aed8e83f57bb321a196367cc9e002a47
shreshth kharbanda <shreshthkharbanda1@gmail.com>
2020-05-28 01:45:57 -0700

    Loads directly to services history screen

--- WebsiteDev/src/App.js
+++ WebsiteDev/src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import LandingPageNavigator from './business/StartingScreens/LandingPage/LandingPageNavigator';
+import PastRequests from './business/BusinessScreens/PastRequests/PastRequests';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 import { fas } from '@fortawesome/free-solid-svg-icons';
+import ServiceHistoryCard from './components/ServiceHistoryCard/ServiceHistoryCard';
 
 library.add(fab, fas);
 
@@ -11,7 +13,7 @@ export default class App extends Component {
 	render() {
 		return (
 			<BrowserRouter>
-				<LandingPageNavigator />
+				<PastRequests />
 			</BrowserRouter>
 		);
 	}
