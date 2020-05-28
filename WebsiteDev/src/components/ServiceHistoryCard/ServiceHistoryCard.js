769f28db6f1cc3ce8166ad725b0855f5d87afa87
shreshth kharbanda <shreshthkharbanda1@gmail.com>
2020-05-28 12:01:24 -0700

    Added stylesheet for ServiceHistoryCard

--- WebsiteDev/src/components/ServiceHistoryCard/ServiceHistoryCard.js
+++ WebsiteDev/src/components/ServiceHistoryCard/ServiceHistoryCard.js
@@ -22,153 +22,76 @@ export default function ServiceHistoryCard(props) {
 
 	// Loads the image (async)
 	useEffect(() => {
-		// loadImage();
+		loadImage();
 	}, []);
 
 	ServiceHistoryCard.propTypes = {
 		height: PropTypes.number,
 		width: PropTypes.number,
-      image: PropTypes.string,
-      service: PropTypes.string,
-      paymentStatus: PropTypes.string,
-      name: PropTypes.string,
-      completedDate: PropTypes.string,
-      total: PropTypes.string,
+		image: PropTypes.string,
+		service: PropTypes.string,
+		paymentStatus: PropTypes.string,
+		name: PropTypes.string,
+		completedDate: PropTypes.string,
+		total: PropTypes.string,
 	};
 
 	const { width, height, service, paymentStatus, name, completedDate, total } = props;
 
 	return (
 		<View>
-			<View
-				style={{
-					paddingStart: '2vh',
-					paddingTop: '2vh',
-					width: '65vw',
-					height: '24vh',
-					borderRadius: 25,
-               borderWidth: 2,
-					borderColor: 'rgba(92, 198, 188, 255)',
-				}}>
-				<View
-					style={{
-						flexDirection: 'row',
-						alignItems: 'flex-start',
-					}}>
-					{' '}
+			<View style={{ ...ServiceHistoryCardStyle.card }}>
+				<View style={{ ...ServiceHistoryCardStyle.row }}>
 					<Text
 						style={{
-                     ...fontStyles.bigSubTitleStyle,
-                     ...fontStyles.darkBlue,
+							...fontStyles.bigSubTitleStyle,
+							...fontStyles.darkBlue,
 						}}>
 						{service}
 					</Text>
-					<View
-						style={{
-                     alignItems: 'flex-start',
-                     marginTop: '0.5vh',
-                     borderRadius: 15,
-                     marginStart: '1vw',
-                     paddingHorizontal: '0.5vw',
-                     paddingVertical: '1vh',
-							'background': 'linear-gradient(90deg, #5cc6bc, #41cbef)',
-						}}>
+					<View style={{ ...ServiceHistoryCardStyle.paymentStatus }}>
 						<Text
 							style={{
-                        ...fontStyles.mainTextStyle,
-                        ...fontStyles.white
+								...fontStyles.mainTextStyle,
+								...fontStyles.white,
 							}}>
 							{paymentStatus}
 						</Text>
 					</View>
 				</View>
-				<View
-					style={{
-						flexDirection: 'row',
-						alignItems: 'flex-start',
-					}}>
-					{' '}
+				<View style={{ ...ServiceHistoryCardStyle.row }}>
 					<Image
-						style={{
-							marginTop: '3vh',
-							width: 119,
-                     height: 119,
-						}}
-						source={require('/Users/shreshthkharbanda/Documents/GitHub/Help/WebsiteDev/src/business/BusinessScreens/PastRequests/guy.png')}
+						style={{ ...ServiceHistoryCardStyle.profileImage }}
+						source={image}
 					/>
-					<View
-						style={{
-							alignItems: 'flex-start',
-							marginStart: '1vw',
-							marginTop: '3vh',
-						}}>
-						<View
-							style={{
-								flexDirection: 'row',
-								alignItems: 'flex-start',
-							}}>
-							{' '}
-							<Text
-								style={{
-                           ...fontStyles.subTextStyle,
-                           ...fontStyles.darkBlue,
-                           marginTop: '2vh'
-                        }}>
+					<View style={{ ...ServiceHistoryCardStyle.firstInfoRow }}>
+						<View style={{ ...ServiceHistoryCardStyle.row }}>
+							<Text style={{ ...ServiceHistoryCardStyle.requestFromText }}>
 								{strings.RequestFromHeader}
 							</Text>
-							<Text
-								style={{
-                           ...fontStyles.bigTextStyle,
-                           ...fontStyles.darkBlue,
-                           ...fontStyles.bold,
-                           marginTop: '2vh',
-                           marginStart: '27vw'
-								}}>
-								{' '}
+							<Text style={{ ...ServiceHistoryCardStyle.completedText }}>
 								{strings.CompletedColon} {completedDate}
 							</Text>
 						</View>
-						<View
-							style={{
-								flexDirection: 'row',
-								alignItems: 'flex-start',
-							}}>
-							{' '}
-							<Text
-								style={{
-                           ...fontStyles.bigSubTitleStyle,
-                           ...fontStyles.darkBlue,
-                           ...fontStyles.bold,
-                           marginStart: '-0.5vw'
-								}}>
-								{name}
-							</Text>
-							<Text
-								style={{
-                           ...fontStyles.bigTextStyle,
-                           ...fontStyles.darkBlue,
-                           ...fontStyles.bold,
-                           marginTop: '2vh',
-                           marginStart: '23.5vw'
-								}}>
-								{' '}
-								{strings.TotalColonDollar}{total}
+						<View style={{ ...ServiceHistoryCardStyle.row }}>
+							<Text style={{ ...ServiceHistoryCardStyle.nameText }}>{name}</Text>
+							<Text style={{ ...ServiceHistoryCardStyle.totalText }}>
+								{strings.TotalColonDollar}
+								{total}
 							</Text>
 						</View>
 					</View>
 				</View>
-				<View
-					style={{
-						alignItems: 'flex-start',
-                  marginStart: '50vw',
-                  marginTop: '-7vh',
-                  height: '3vh',
-					}}>
-					<HelpButton title='View More' width='10vw' onPress={() =>
-						history.push({
-							pathname: '/viewrequest',
-						})
-					}/>
+				<View style={{ ...ServiceHistoryCardStyle.viewMoreButton }}>
+					<HelpButton
+						title='View More'
+						width='10vw'
+						onPress={() =>
+							history.push({
+								pathname: '/viewrequest',
+							})
+						}
+					/>
 				</View>
 			</View>
 		</View>
--- WebsiteDev/src/components/ServiceHistoryCard/ServiceHistoryCardStyle.js
+++ WebsiteDev/src/components/ServiceHistoryCard/ServiceHistoryCardStyle.js
@@ -4,7 +4,7 @@ import colors from '../../config/colors';
 import { screenHeight, screenWidth } from '../../config/dimensions';
 import fontStyles from '../../config/fontStyles';
 
-export default StyleSheet.create({
+export default {
 	card: {
       paddingStart: '2vh',
       paddingTop: '2vh',
@@ -12,9 +12,60 @@ export default StyleSheet.create({
       height: '24vh',
       borderRadius: 25,
       borderWidth: 2,
+      'borderColor': 'rgba(92, 198, 188, 255)',
    },
    row: {
       flexDirection: 'row',
       alignItems: 'flex-start',
+   },
+   paymentStatus: {
+      alignItems: 'flex-start',
+      marginTop: '0.5vh',
+      borderRadius: 15,
+      marginStart: '1vw',
+      paddingHorizontal: '0.5vw',
+      paddingVertical: '1vh',
+      'background': 'linear-gradient(90deg, #5cc6bc, #41cbef)',
+   },
+   profileImage: {
+      marginTop: '4vh',
+      width: '5vw',
+      height: '5vw',
+   },
+   firstInfoRow: {
+      alignItems: 'flex-start',
+      marginStart: '1vw',
+      marginTop: '3vh',
+   },
+   viewMoreButton: {
+      alignItems: 'flex-start',
+      marginStart: '50vw',
+      marginTop: '-7vh',
+      height: '3vh',
+   },
+   requestFromText: {
+      ...fontStyles.subTextStyle,
+      ...fontStyles.darkBlue,
+      marginTop: '2vh',
+   },
+   completedText: {
+      ...fontStyles.bigTextStyle,
+      ...fontStyles.darkBlue,
+      ...fontStyles.bold,
+      marginTop: '2vh',
+      marginStart: '27vw',
+   },
+   nameText: {
+      ...fontStyles.bigSubTitleStyle,
+      ...fontStyles.darkBlue,
+      ...fontStyles.bold,
+      marginStart: '-0.5vw',
+   },
+   totalText: {
+      ...fontStyles.bigTextStyle,
+      ...fontStyles.darkBlue,
+      ...fontStyles.bold,
+      marginTop: '2vh',
+      marginStart: '23.5vw',
    }
-});
+}
