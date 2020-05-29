cbc0c5c5f706f6834ce5ae71efc72a04915a2631
shreshth kharbanda <shreshthkharbanda1@gmail.com>
2020-05-28 23:15:29 -0700

    Reset App.js

--- WebsiteDev/src/App.js
+++ WebsiteDev/src/App.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import LandingPageNavigator from './business/StartingScreens/LandingPage/LandingPageNavigator';
-import PastRequests from './business/BusinessScreens/PastRequests/PastRequests';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 import { fas } from '@fortawesome/free-solid-svg-icons';
-import ServiceHistoryCard from './components/ServiceHistoryCard/ServiceHistoryCard';
 
 library.add(fab, fas);
 
@@ -13,7 +11,7 @@ export default class App extends Component {
 	render() {
 		return (
 			<BrowserRouter>
-				<PastRequests />
+				<LandingPageNavigator />
 			</BrowserRouter>
 		);
 	}
