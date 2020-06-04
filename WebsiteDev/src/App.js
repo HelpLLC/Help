97c781eedc43aa7b755187197c630a62286dc8ea
khalidelho <47620535+khalidelho@users.noreply.github.com>
2020-06-03 19:21:22 -0700

    comitting to merge

--- WebsiteDev/src/App.js
+++ WebsiteDev/src/App.js
@@ -5,16 +5,17 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import FirebaseFunctions from './config/FirebaseFunctions';
-
+import ServiceScreen from './business/BusinessScreens/ServiceScreen/ServiceScreen';
 
 library.add(fab, fas);
 
 export default class App extends Component {
 	render() {
 		return (
-			<BrowserRouter>
-			<LandingPageNavigator />
-		</BrowserRouter>
+			<ServiceScreen/>
+		// 	<BrowserRouter>
+		// 	<LandingPageNavigator />
+		// </BrowserRouter>
 		);
 	}
 }
