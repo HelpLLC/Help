776ffa0655b8cc96b402c944cdfb96fe4223e7c2
shreshth kharbanda <shreshthkharbanda1@gmail.com>
2020-06-07 16:14:21 -0700

    Made the Employee List Item component

---
+++ WebsiteDev/src/components/EmployeeListItem/EmployeeListItemStyle.js
@@ -0,0 +1,41 @@
+//This is the styles for the HelpTextInput component
+import { StyleSheet } from 'react-native';
+import colors from '../../config/colors';
+import { screenHeight, screenWidth } from '../../config/dimensions';
+import fontStyles from '../../config/fontStyles';
+
+export default {
+	listItem: {
+		marginTop: '2vh',
+		display: 'flex',
+		width: '65vw',
+		justifyContent: 'space-between',
+		flexDirection: 'row',
+		borderRadius: '30px',
+		border: '2px solid #5cc6bc',
+		'box-shadow': '0px 3px 5px 1px rgba(92, 198, 188, 2.55)',
+	},
+	row: {
+		display: 'flex',
+		flexDirection: 'row',
+	},
+	profileImage: {
+		marginLeft: '0.5vw',
+		marginTop: '0.5vh',
+		marginBottom: '0.5vh',
+		width: '5vw',
+		height: '5vw',
+	},
+	nameText: {
+		...fontStyles.bigSubTitleStyle,
+		...fontStyles.darkBlue,
+		...fontStyles.bold,
+		marginLeft: '1vw',
+		marginTop: '2vh',
+	},
+	assignButton: {
+		marginTop: '-4vh',
+		alignItems: 'center',
+		marginRight: '1vw',
+	},
+};
