0984bcffd10d9d01ddf5f42cc5bac76b77c3596a
Zyad <zyadgohary@hotmail.com>
2020-07-02 13:19:05 -0700

    Completed #471

--- WebsiteDev/src/business/BusinessScreens/Dashboard/Dashboard.js
+++ WebsiteDev/src/business/BusinessScreens/Dashboard/Dashboard.js
@@ -4,28 +4,30 @@ import '../Header/Header.css';
 import FirebaseFunctions from '../../../config/FirebaseFunctions';
 import BusinessServiceCard from '../../../components/BusinessServiceCard/BusinessServiceCard';
 import { useLocation, useHistory } from 'react-router-dom';
+import colors from '../../../config/colors';
 import '../../../config/fontStyles.css';
 import HelpButton from '../../../components/HelpButton/HelpButton';
-import UpcomingRequests from '../../../components/UpcomingRequests/UpcomingRequests';
-import SideMenu from '../../../components/SideMenu/SideMenu';
+import SideMenuCard from '../../../components/SideMenu/SideMenuCard';
+import DropdownHeader from '../../../components/DropdownHeader/DropdownHeader';
 import ReactLoading from 'react-loading';
-import colors from '../../../config/colors';
+import strings from '../../../config/strings';
+import fontStyles from '../../../config/fontStyles';
 
 export default function Dashboard(props) {
-	// The state variables for this screen
 	const [business, setBusiness] = useState();
 	const [services, setServices] = useState();
-	const [unconfirmedRequests, setUnconfirmedRequests] = useState('');
 	const [isLoading, setIsLoading] = useState(true);
+	const [unconfirmedRequests, setUnconfirmedRequests] = useState('');
+	const [unconfirmedRequestsImages, setUnconfirmedRequestsImages] = useState('');
 	const location = useLocation();
 	const history = useHistory();
 
-	// The useEffect method for this screen
+	// The useEffect method fetches the correct information
 	useEffect(() => {
 		fetchFunc();
 	}, []);
 
-	// helper method for the useEffect because it can't be async
+	// Helper method for the useEffect method
 	const fetchFunc = async () => {
 		const businessID = location.state.businessID;
 		const results = await Promise.all([
@@ -36,6 +38,16 @@ export default function Dashboard(props) {
 		]);
 		const business = results[0];
 		const unconfirmedRequests = results[1];
+
+		// Sets the unconfimred request images
+		let imagePromises = unconfirmedRequests.map((eachRequest) =>
+			FirebaseFunctions.call('getServiceImageByID', {
+				serviceID: eachRequest.serviceID,
+			})
+		);
+		imagePromises = await Promise.all(imagePromises);
+
+		setUnconfirmedRequestsImages(imagePromises);
 		setBusiness(business);
 		setUnconfirmedRequests(unconfirmedRequests);
 		setServices(business.services);
@@ -56,7 +68,7 @@ export default function Dashboard(props) {
 			<section className='dropdownheader'>
 				<DropdownHeader
 					businessID={business}
-					businessName={businessName}
+					businessName={business.businessName}
 					modalClassName='modal'
 					divClassName='toprightcontainer'
 				/>
@@ -83,33 +95,105 @@ export default function Dashboard(props) {
 								}}
 							/>
 						</div>
-						{loaded
-							? services.map((service) => (
-									<BusinessServiceCard
-										title={service.serviceTitle}
-										totalReviews={service.totalReviews}
-										averageRating={service.averageRating}
-										priceText={service.priceText}
-										serviceDescription={service.serviceDescription}
-										numCurrentRequests={service.numCurrentRequests}
-										image={async () => {
-											//Passes in the function to retrieve the image of this product
-											return await FirebaseFunctions.call('getServiceImageByID', {
-												serviceID: service.serviceID,
-											});
-										}}
-										onPress={() => {
-											history.push({
-												pathname: '/serviceScreen',
-												state: { business: business, service: service },
-											});
-										}}
-									/>
-							  ))
-							: null}
+						{services.map((service) => (
+							<BusinessServiceCard
+								title={service.serviceTitle}
+								totalReviews={service.totalReviews}
+								averageRating={service.averageRating}
+								priceText={service.priceText}
+								serviceDescription={service.serviceDescription}
+								numCurrentRequests={service.numCurrentRequests}
+								image={async () => {
+									//Passes in the function to retrieve the image of this product
+									return await FirebaseFunctions.call('getServiceImageByID', {
+										serviceID: service.serviceID,
+									});
+								}}
+								onPress={() => {
+									history.push({
+										pathname: '/serviceScreen',
+										state: { business: business, service: service },
+									});
+								}}
+							/>
+						))}
 					</div>
 					<div className='upcomingRequestsContainer'>
-						<UpcomingRequests />
+						{unconfirmedRequests.map((eachRequest, index) => {
+							let date = new Date(eachRequest.date);
+							date =
+								['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'][date.getDay()] +
+								', ' +
+								[
+									'Jan',
+									'Feb',
+									'Mar',
+									'Apr',
+									'May',
+									'Jun',
+									'Jul',
+									'Aug',
+									'Sep',
+									'Oct',
+									'Nov',
+									'Dec',
+								][date.getMonth()] +
+								' ' +
+								date.getDate() +
+								', ' +
+								date.getFullYear();
+							return (
+								<div className='cardContainer'>
+									<img className={'imageStyle'} src={unconfirmedRequestsImages[index].uri}></img>
+									<div className='unconfirmedInfoCard'>
+										<div className={'darkBlue bold mainTextStyle'}>{eachRequest.serviceTitle}</div>
+										<div className={'darkBlue bold smallTextStyle'}>{date}</div>
+										<div className={'darkBlue bold smallTextStyle'}>
+											{eachRequest.time} - {eachRequest.endTime}
+										</div>
+										<HelpButton
+											title={strings.ViewMore}
+											fontStyle={{
+												...fontStyles.subTextStyle,
+												...fontStyles.white,
+												...fontStyles.bold,
+											}}
+											height={'3vh'}
+											width={'10vw'}
+											onPress={() => {
+												//Goes to the specific request screen
+												history.push({
+													pathname: '/viewrequest',
+													state: { requestID: eachRequest.requestID },
+												});
+											}}
+										/>
+									</div>
+								</div>
+							);
+						})}
+						<div className={'viewAllButton'}>
+							<HelpButton
+								title={strings.ViewAll}
+								fontStyle={{
+									...fontStyles.subTextStyle,
+									...fontStyles.white,
+									...fontStyles.bold,
+								}}
+								height={'4vh'}
+								width={'27vw'}
+								onPress={() => {
+									//Goes to the specific unconfirmedreqeusts  screen
+									history.push({
+										pathname: '/viewAll',
+										state: {
+											business: business,
+											title: strings.UnconfirmedRequests,
+										},
+									});
+								}}
+							/>
+						</div>
 					</div>
 				</div>
 			</section>
