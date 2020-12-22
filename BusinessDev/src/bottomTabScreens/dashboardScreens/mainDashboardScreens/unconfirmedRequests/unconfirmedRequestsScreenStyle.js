fe4abb89755cc248cf9628d6eff1420216a49ce2
Allan <allandao05@gmail.com>
2020-12-22 15:59:57 -0800

    Fix rendering of unconfirmed request cards

--- BusinessDev/src/bottomTabScreens/dashboardScreens/mainDashboardScreens/unconfirmedRequests/unconfirmedRequestsScreenStyle.js
+++ BusinessDev/src/bottomTabScreens/dashboardScreens/mainDashboardScreens/unconfirmedRequests/unconfirmedRequestsScreenStyle.js
@@ -4,42 +4,47 @@ import { screenWidth, screenHeight } from 'config/dimensions';
 import colors from 'config/colors';
 
 export default StyleSheet.create({
-  AddServiceButton: {
-    marginLeft: screenWidth * 0.05,
-    marginTop: screenHeight * 0.05,
-  },
-  ServiceCard: {
-    width: screenWidth * 0.95,
-    height: screenHeight * 0.22,
-    borderColor: colors.lightBlue,
-    borderWidth: 3,
-    borderRadius: 25,
-    flexDirection: 'row',
-    alignSelf: 'center',
-    justifyContent: 'center',
-    marginTop: screenHeight * 0.05,
-  },
-  imageStyle: {
-    width: screenWidth * 0.4 - 3,
-    height: screenHeight * 0.13 - 3,
-    borderRadius: 25,
-  },
-  TextPlusButton: {
-    marginTop: screenHeight * 0.05,
-  },
-  MiddleText: {
-    marginTop: screenHeight * 0.02,
-  },
-  ImageandTitle: {
-    marginTop: screenHeight * 0.02,
-  },
-  BottomSpacer: {
-    marginBottom: screenHeight * 0.02,
-  },
-  RightSpacer: {
-    marginRight: screenWidth * 0.02,
-  },
-  TopSpacer: {
-    marginTop: screenHeight * 0.005,
-  },
+	AddServiceButton: {
+		marginLeft: screenWidth * 0.05,
+		marginTop: screenHeight * 0.05,
+	},
+	ServiceCard: {
+		width: screenWidth * 0.95,
+		height: screenHeight * 0.25,
+		borderColor: colors.lightBlue,
+		borderWidth: 3,
+		borderRadius: 25,
+		flexDirection: 'row',
+		alignSelf: 'center',
+		justifyContent: 'center',
+		marginTop: screenHeight * 0.05,
+	},
+	imageStyle: {
+		width: screenWidth * 0.4 - 3,
+		height: screenHeight * 0.13 - 3,
+		borderRadius: 25,
+	},
+	TextPlusButton: {
+		marginTop: screenHeight * 0.05,
+		marginLeft: screenWidth * 0.02,
+	},
+	MiddleText: {
+		marginTop: screenHeight * 0.03,
+	},
+	ImageandTitle: {
+		marginLeft: screenWidth * -0.05,
+		marginTop: screenHeight * 0.02,
+	},
+	BottomSpacer: {
+		marginBottom: screenHeight * 0.02,
+	},
+	RightSpacer: {
+		marginRight: screenWidth * 0.02,
+	},
+	TopSpacer: {
+		marginTop: screenHeight * 0.005,
+	},
+	EditButton: {
+		marginRight: screenWidth * -0.25,
+	},
 });
