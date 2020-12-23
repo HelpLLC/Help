65eba7ba191be198fece4a76c1a592bcab663ff7
Allan <allandao05@gmail.com>
2020-12-22 16:01:28 -0800

    Fix position of edit button (not showing before)

--- BusinessDev/src/bottomTabScreens/dashboardScreens/mainDashboardScreens/services/serviceScreenStyle.js
+++ BusinessDev/src/bottomTabScreens/dashboardScreens/mainDashboardScreens/services/serviceScreenStyle.js
@@ -4,48 +4,48 @@ import { screenWidth, screenHeight } from 'config/dimensions';
 import colors from 'config/colors';
 
 export default StyleSheet.create({
-  AddServiceButton: {
-    marginLeft: screenWidth * 0.05,
-    marginTop: screenHeight * 0.05,
-  },
-  ServiceCard: {
-    width: screenWidth * 0.95,
-    height: screenHeight * 0.25,
-    borderColor: colors.lightBlue,
-    borderWidth: 3,
-    borderRadius: 25,
-    flexDirection: 'row',
-    alignSelf: 'center',
-    justifyContent: 'center',
-    marginTop: screenHeight * 0.05,
-  },
-  imageStyle: {
-    width: screenWidth * 0.4 - 3,
-    height: screenHeight * 0.13 - 3,
-    borderRadius: 25,
-  },
-  TextPlusButton: {
-    marginTop: screenHeight * 0.05,
-    marginLeft: screenWidth * 0.05
-  },
-  MiddleText: {
-    marginTop: screenHeight * 0.03,
-  },
-  ImageandTitle: {
-    marginLeft: screenWidth * -0.1,
-    marginTop: screenHeight * 0.02,
-  },
-  BottomSpacer: {
-    marginBottom: screenHeight * 0.02,
-  },
-  RightSpacer: {
-    marginRight: screenWidth * 0.02,
-  },
-  TopSpacer: {
-    marginTop: screenHeight * 0.005,
-  },
-  EditButton: {
-    marginRight: screenWidth * -0.3,
-    marginTop: screenHeight * 0.03,
-  },
+	AddServiceButton: {
+		flexDirection: 'row',
+		alignSelf: 'center',
+		marginTop: screenHeight * 0.05,
+	},
+	ServiceCard: {
+		width: screenWidth * 0.95,
+		height: screenHeight * 0.25,
+		borderColor: colors.lightBlue,
+		borderWidth: 3,
+		borderRadius: 25,
+		flexDirection: 'row',
+		alignSelf: 'center',
+		justifyContent: 'center',
+		marginTop: screenHeight * 0.05,
+	},
+	imageStyle: {
+		width: screenWidth * 0.4 - 3,
+		height: screenHeight * 0.13 - 3,
+		borderRadius: 25,
+	},
+	TextPlusButton: {
+		marginTop: screenHeight * 0.05,
+		marginLeft: screenWidth * 0.05,
+	},
+	MiddleText: {
+		marginTop: screenHeight * 0.03,
+	},
+	ImageandTitle: {
+		marginLeft: screenWidth * -0.1,
+		marginTop: screenHeight * 0.02,
+	},
+	BottomSpacer: {
+		marginBottom: screenHeight * 0.02,
+	},
+	RightSpacer: {
+		marginRight: screenWidth * 0.02,
+	},
+	TopSpacer: {
+		marginTop: screenHeight * 0.005,
+	},
+	EditButton: {
+		marginRight: screenWidth * -0.3,
+	},
 });
