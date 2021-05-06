9656d36b18c000ae4df13cc0c4cc85ab1d93c7e6
jordanp325 <jordanp325@outlook.com>
2021-05-05 21:27:50 -0700

    Handling profile pictures

--- BusinessDev/src/employeeScreens/employeeBottomTabScreens/employeeProfile/employeeProfileScreen.js
+++ BusinessDev/src/employeeScreens/employeeBottomTabScreens/employeeProfile/employeeProfileScreen.js
@@ -9,85 +9,155 @@ import { screenWidth, screenHeight } from 'config/dimensions';
 import HelpButton from '../../../components/HelpButton/HelpButton';
 import style from './employeeProfileScreenStyle';
 import ServiceInfoCard from '../../../components/ServiceInfoCard/ServiceInfoCard';
+import LoadingSpinner from '../../../components/LoadingSpinner';
 import { Icon } from 'react-native-elements';
 import colors from 'config/colors';
 
 const employeeProfileScreen = (props) => {
-  const options = [
-    strings.ProfileCompanyInfo,
-    strings.ProfileBusinessSchedule,
-    strings.ProfilePassword,
-    strings.ProfileLogout,
-  ];
+	// props = {navigation:{push:()=>{},state:{params:{businessID:'zjCzqSiCpNQELwU3ETtGBANz7hY2'}}}}; //NOTE: this line is only for testing
 
-  function renderOptions() {
-    let elements = [];
-    for (let i in options)
-      elements.push(
-        <TouchableOpacity
-          style={
-            options[i] == strings.ProfileLogout ? style.LogoutPositioning : null
-          }
-          key={options[i]}
-        >
-          <View
-            style={[
-              style.OptionContainer,
-              options[i] == strings.ProfileLogout
-                ? style.LogoutContainer
-                : style.OptionNormal,
-            ]}
-          >
-            <Text
-              style={[
-                fontStyles.mainTextStyle,
-                style.OptionText,
-                options[i] == strings.ProfileLogout ? style.LogoutText : {},
-              ]}
-            >
-              {options[i]}
-            </Text>
-            {options[i] == strings.ProfileLogout ? null : (
-              <Icon
-                name={'angle-right'}
-                type='font-awesome'
-                size={25}
-                color={
-                  options[i] == strings.ProfileLogout
-                    ? colors.white
-                    : colors.darkBlue
-                }
-              />
-            )}
-          </View>
-        </TouchableOpacity>
-      );
-    return elements;
-  }
+	const options = [
+		strings.ProfileCompanyInfo,
+		strings.ProfileBusinessSchedule,
+		strings.ProfilePassword,
+		strings.ProfileLogout,
+	];
+	//the state of the function
+	const [employeeID, setEmployeeID] = useState('');
+	const [profilePicture, setProfilePicture] = useState('');
+	const [isScreenLoading, setIsScreenLoading] = useState(true);
+	const [imagePickerVisible, setImagePickerVisible] = useState(false);
 
-  return (
-    <HelpView>
-      <TopBanner
-        title={strings.Dashboard}
-        size={30}
-        leftOnPress={() => props.navigation.goBack()}
-      />
-      <View style={style.Body}>
-        <View style={style.ContentContainer}>
-          <View style={style.MainContainer}>
-            <Image
-              source={require('../../../bottomTabScreens/profile/profileScreen/profilePicture.png')}
-              style={{ width: 100, height: 100, margin: 10 }}
-            />
-            <Text style={[fontStyles.mainTextStyle, style.MainText]}>
-              Employee
-            </Text>
-          </View>
-          {renderOptions()}
-        </View>
-      </View>
-    </HelpView>
-  );
+	async function getData() {
+		//Declares the screen name in Firebase
+		FirebaseFunctions.setCurrentScreen('ProfileScreen', 'profileScreen');
+		const { employeeID: EID } = props.navigation.state.params;
+
+		// const businessObj = await FirebaseFunctions.call('getBusinessByID', {
+		// 	businessID: BID,
+		// });
+
+		const profilePic = await FirebaseFunctions.call('getEmployeeProfilePictureByID', {
+			employeeID: EID,
+		});
+
+		setEmployeeID(EID);
+		setProfilePicture(profilePic);
+		setIsScreenLoading(false);
+	}
+	useEffect(() => {
+		getData();
+	}, []);
+
+	function renderOptions() {
+		let elements = [];
+		for (let i in options)
+			elements.push(
+				<TouchableOpacity
+					style={
+						options[i] == strings.ProfileLogout ? style.LogoutPositioning : null
+					}
+					key={options[i]}
+				>
+					<View
+						style={[
+							style.OptionContainer,
+							options[i] == strings.ProfileLogout
+								? style.LogoutContainer
+								: style.OptionNormal,
+						]}
+					>
+						<Text
+							style={[
+								fontStyles.mainTextStyle,
+								style.OptionText,
+								options[i] == strings.ProfileLogout ? style.LogoutText : {},
+							]}
+						>
+							{options[i]}
+						</Text>
+						{options[i] == strings.ProfileLogout ? null : (
+							<Icon
+								name={'angle-right'}
+								type='font-awesome'
+								size={25}
+								color={
+									options[i] == strings.ProfileLogout
+										? colors.white
+										: colors.darkBlue
+								}
+							/>
+						)}
+					</View>
+				</TouchableOpacity>
+			);
+		return elements;
+	}
+
+	if (isScreenLoading)
+		return (
+			<View style={style.Body}>
+				<View style={{ justifyContent: 'center', alignItems: 'center', flex: 1 }}>
+					<LoadingSpinner isVisible={true} />
+				</View>
+			</View>
+		);
+	else return (
+		<HelpView>
+			<TopBanner
+				title={strings.Dashboard}
+				size={30}
+				leftOnPress={() => props.navigation.goBack()}
+			/>
+			<View style={style.Body}>
+				<View style={style.ContentContainer}>
+					<View style={style.MainContainer}>
+						<TouchableOpacity onPress={()=>{
+							setImagePickerVisible(true);
+						}}>
+							<Image
+								source={profilePicture}
+								style={{ width: 100, height: 100, margin: 10 }}
+							/>
+						</TouchableOpacity>
+						<Text style={[fontStyles.mainTextStyle, style.MainText]}>
+							Employee
+            			</Text>
+					</View>
+					{renderOptions()}
+					<ImagePicker
+						imageHeight={256}
+						imageWidth={256}
+						onImageCanceled={() => {
+							setImagePickerVisible(false);
+						}}
+						onImageSelected={(response) => {
+							this.setState({ isShowing: false });
+							const source = { uri: 'data:image/jpeg;base64,' + response.data };
+							if (!(source.uri === 'data:image/jpeg;base64,undefined')) {
+
+								let absolutePath = '';
+								if (Platform.OS === 'android') {
+									absolutePath = 'file://' + response.path;
+								} else {
+									absolutePath = response.path;
+								}
+								//Creates the reference & uploads the image (async)
+								await FirebaseFunctions.storage
+									.ref('employeeProfilePics/' + employeeID)
+									.putFile(absolutePath);
+								
+								setProfilePicture(await FirebaseFunctions.call('getEmployeeProfilePictureByID', {employeeID}));
+
+							}
+							setImagePickerVisible(false);
+						}}
+						isShowing={imagePickerVisible}
+					/>
+				</View>
+			</View>
+		</HelpView>
+	);
 };
 
 export default employeeProfileScreen;
