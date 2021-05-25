5196274423cd6ce6bddd886d11dd096944cc772d
jordanp325 <jordanp325@outlook.com>
2021-05-24 17:42:41 -0700

    Update App.js

--- WebsiteDev/src/App.js
+++ WebsiteDev/src/App.js
@@ -1,4 +1,4 @@
-/*
+//*
 import React, { useEffect } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import LandingPageNavigator from './business/StartingScreens/LandingPage/LandingPageNavigator';
